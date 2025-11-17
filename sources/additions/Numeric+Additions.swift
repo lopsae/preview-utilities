6630de033ce3c4f036f82bdcc73d73d9f6b2c432
Maic Lopez Saenz <maic@lopsae.com>
2025-11-17 11:58:38 -0800

    add int.clamp

--- sources/additions/Numeric+Additions.swift
+++ sources/additions/Numeric+Additions.swift
@@ -7,6 +7,16 @@
 import CoreFoundation
 
 
+extension Numeric where Self == Int {
+
+    @inlinable public func clamped(to range: Range<Self>) -> Self {
+        // TODO: test return for empty ranges
+        Swift.max(range.lowerBound, Swift.min(self, range.upperBound - 1))
+    }
+
+}
+
+
 extension Numeric where Self == Double {
 
     @inlinable public var toInt: Int {
