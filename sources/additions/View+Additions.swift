e148d43858a06c47933128c9f031c0fbac23b0a6
Maic Lopez Saenz <maic@lopsae.com>
2025-11-19 15:01:41 -0800

    make public View additions

--- sources/additions/View+Additions.swift
+++ sources/additions/View+Additions.swift
@@ -9,22 +9,22 @@ import SwiftUI
 
 extension View {
 
-    func maxWidthFrame() -> some View {
+    public func maxWidthFrame() -> some View {
         self.frame(maxWidth: .infinity)
     }
 
 
-    func maxHeightFrame() -> some View {
+    public func maxHeightFrame() -> some View {
         self.frame(maxHeight: .infinity)
     }
 
 
-    func maxSizeFrame() -> some View {
+    public func maxSizeFrame() -> some View {
         self.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 
 
-    func frame(square side: CGFloat) -> some View {
+    public func frame(square side: CGFloat) -> some View {
         self.frame(width: side, height: side)
     }
 
