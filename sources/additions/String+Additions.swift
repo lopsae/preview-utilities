fb5a878baa6683ed3ee2d61609267c72d4e17316
Maic Lopez Saenz <maic@lopsae.com>
2025-11-21 00:45:03 -0800

    make String additions public

--- sources/additions/String+Additions.swift
+++ sources/additions/String+Additions.swift
@@ -6,20 +6,20 @@
 
 extension String {
 
-    static let alphabet: [String] = [
+    public static let alphabet: [String] = [
         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
         "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
     ]
 
 
-    static let natoPhoneticAlphabet: [String] = [
+    public static let natoPhoneticAlphabet: [String] = [
         "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India",
         "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo",
         "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"
     ]
 
 
-    static let sphinxOfBlackQuartz: String = "sphinx of black quartz, judge my vow"
+    public static let sphinxOfBlackQuartz: String = "sphinx of black quartz, judge my vow"
 
 }
 
