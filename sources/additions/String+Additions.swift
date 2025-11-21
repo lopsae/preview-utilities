8346b47f802415c0e42391786eb275e0a75d5b64
Maic Lopez Saenz <maic@lopsae.com>
2025-11-21 00:05:54 -0800

    add Strings+Additions

---
+++ sources/additions/String+Additions.swift
@@ -0,0 +1,25 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+extension String {
+
+    static let alphabet: [String] = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    ]
+
+
+    static let natoPhoneticAlphabet: [String] = [
+        "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India",
+        "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo",
+        "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"
+    ]
+
+
+    static let sphinxOfBlackQuartz: String = "sphinx of black quartz, judge my vow"
+
+}
+
