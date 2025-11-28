164553c888e26578b9f9026975b8303659bd4830
Maic Lopez Saenz <maic@lopsae.com>
2025-11-28 12:08:58 -0800

    add NSParagraphStyle additions

---
+++ sources/additions/NSParagraphStyle+Additions.swift
@@ -0,0 +1,33 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+#if canImport(AppKit)
+import AppKit
+#elseif canImport(UIKit)
+import UIKit
+#endif
+
+
+extension NSParagraphStyle {
+
+    public static func make(
+        mutate: (NSMutableParagraphStyle) -> Void
+    ) -> NSParagraphStyle {
+        let mutable = NSMutableParagraphStyle(mutate: mutate)
+        return mutable
+    }
+
+}
+
+
+extension NSMutableParagraphStyle {
+
+    public convenience init(mutate: (Self) -> Void) {
+        self.init()
+        mutate(self)
+    }
+
+}
