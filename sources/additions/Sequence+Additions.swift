ff667c08f26cc76f422f06e3826b8dd01e11fa5f
Maic Lopez Saenz <maic@lopsae.com>
2025-11-28 13:59:00 -0800

    add `sequence.dictionaryMap`s

--- sources/additions/Sequence+Additions.swift
+++ sources/additions/Sequence+Additions.swift
@@ -4,6 +4,49 @@
 //
 
 
+extension Sequence {
+
+    // TODO: note in documentation that per `Dictionary(uniqueKeysWithValues`, passing a duplicate key results in a runtime error.
+    @inlinable public func dictionaryMap<Key: Hashable, Value>(
+        key keyTransform: (Element) -> Key,
+        value valueTransform: (Element) -> Value
+    ) -> [Key: Value] {
+        let tuples = self.map { (keyTransform($0), valueTransform($0)) }
+        return Dictionary(uniqueKeysWithValues: tuples)
+    }
+
+
+    @inlinable public func dictionaryMap<Key: Hashable, Value>(
+        key keyTransform: (Element) -> Key,
+        value: Value
+    ) -> [Key: Value] {
+        let tuples = self.map { (keyTransform($0), value) }
+        return Dictionary(uniqueKeysWithValues: tuples)
+    }
+
+}
+
+
+extension Sequence where Element: Hashable {
+
+    // TODO: note in documentation that per `Dictionary(uniqueKeysWithValues`, passing a duplicate key results in a runtime error.
+    @inlinable public func dictionaryMap<Value>(
+        value valueTransform: (Element) -> Value
+    ) -> [Element: Value] {
+        let tuples = self.map { ($0, valueTransform($0)) }
+        return Dictionary(uniqueKeysWithValues: tuples)
+    }
+
+
+    @inlinable public func dictionaryMap<Value>(
+        value: Value
+    ) -> [Element: Value] {
+        let tuples = self.map { ($0, value) }
+        return Dictionary(uniqueKeysWithValues: tuples)
+    }
+}
+
+
 extension Sequence where Element: OptionSet {
 
     /// Returns a new option set with the union of all elements contained in this set.
