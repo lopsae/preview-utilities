82344c3b008fb5ce888ab3d43c8d38b52bca1efd
Maic Lopez Saenz <maic@lopsae.com>
2025-11-29 13:41:31 -0800

    add additions for CGRect

--- sources/additions/CGRect+Additions.swift
+++ sources/additions/CGRect+Additions.swift
@@ -5,18 +5,34 @@
 
 
 import CoreGraphics
+import SwiftUI
 
 
 extension CGRect {
 
-    @inlinable var center: CGPoint {
+    @inlinable public func set(
+        x newX: CGFloat? = nil,
+        y newY: CGFloat? = nil,
+        width newWidth: CGFloat? = nil,
+        height newHeight: CGFloat? = nil
+    ) -> Self {
+        var newRect = self
+        if let newX { newRect.origin.x = newX }
+        if let newY { newRect.origin.y = newY }
+        if let newWidth { newRect.size.width  = newWidth }
+        if let newHeight { newRect.size.height = newHeight }
+        return newRect
+    }
+
+
+    @inlinable public var center: CGPoint {
         size.toPoint
             .times(by: 0.5)
             .offset(by: origin)
     }
 
 
-    @inlinable func center(size: CGSize) -> CGRect {
+    @inlinable public func center(size: CGSize) -> Self {
         let centeredRect = CGRect(
             x: (self.width - size.width) / 2 + self.origin.x,
             y: (self.height - size.height) / 2 + self.origin.y,
@@ -24,6 +40,11 @@ extension CGRect {
             height: size.height
         )
         return centeredRect
+    }
+
+
+    @inlinable public func offset(x: CGFloat = 0, y: CGFloat = 0) -> Self {
+        self.offsetBy(dx: x, dy: y)
     }
 
 }
@@ -55,3 +76,16 @@ extension CGRect {
 }
 
 #endif
+
+
+// MARK:- Interactions with Path.
+
+extension CGRect {
+
+    @discardableResult
+    @inlinable public func addTo(path: inout Path) -> Self {
+        path.addRect(self)
+        return self
+    }
+
+}
