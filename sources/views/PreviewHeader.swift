79243abdcc82918c2dfb7b6a3b2251051bff28cd
Maic Lopez Saenz <maic@lopsae.com>
2025-12-08 02:03:35 -0800

    cleanup PreviewHeader padding calculation

--- sources/views/PreviewHeader.swift
+++ sources/views/PreviewHeader.swift
@@ -9,26 +9,36 @@ import SwiftUI
 
 struct PreviewHeader: View {
 
-    @State private var heightPadded: Double = 0
-    @State private var heightComplete: Double = 0
-    @State private var headerTopPadding: Double = 0
+    @State private var paddedHeight: Double = 0
+    @State private var fullHeight: Double = 0
+    @State private var topSafeArea: Double = 0
 
-    let flexibleSize: Bool = true
+    let flexibleSize: Bool
+
+    fileprivate var printsUpdates: Bool = false
+
+
+    init(flexibleSize: Bool = true) {
+        self.flexibleSize = flexibleSize
+    }
+
 
     var body: some View {
         VStack(spacing: 0) {
 
             Text("Header")
                 .foregroundStyle(.tertiary)
+                .padding(.top, textTopPadding)
                 // Double padding to separate one padding from background,
                 // which is padded once from views edge.
-                .padding(.top, headerTopPadding)
                 .padding(.bottom)
                 .padding(.bottom)
                 .maxWidthFrame()
                 .onGeometryChange(of: \.safeAreaInsets.top) { newTopSafeArea in
-                    let topPadding = minHeaderTopPadding - newTopSafeArea
-                    headerTopPadding = max(0, topPadding)
+                    if printsUpdates {
+                        print("update topSafeArea:\(newTopSafeArea)")
+                    }
+                    topSafeArea = newTopSafeArea
                 }
 
             if flexibleSize {
@@ -40,18 +50,42 @@ struct PreviewHeader: View {
             ConcentricRectangle(corners: .concentric(minimum: .fixed(HeaderFooterPreview<EmptyView>.minConcentricRoundedCornerRadius)))
                 .fill(.gray.tertiary)
                 .onGeometryChange(of: \.size.height) { newHeight in
-                    heightPadded = newHeight
+                    if printsUpdates {
+                        print("update paddedHeight:\(newHeight)")
+                    }
+                    paddedHeight = newHeight
                 }
                 .padding()
                 .onGeometryChange(of: \.size.height) { newHeight in
-                    heightComplete = newHeight
+                    if printsUpdates {
+                        print("update fullHeight:\(newHeight)")
+                    }
+                    fullHeight = newHeight
                 }
                 .ignoresSafeArea()
         }  // background
     }
 
-    private var minHeaderTopPadding: Double {
-        (heightComplete - heightPadded) * 1.5 / 2.0
+
+    private var textTopPadding: Double {
+        let onePadding = (fullHeight - paddedHeight) / 2.0
+        let minPadding = onePadding * 1.5
+        let possiblePadding = minPadding - topSafeArea
+        return max(0, possiblePadding)
+
+    }
+
+}
+
+
+// MARK: - Preview utilities.
+
+extension PreviewHeader {
+
+    fileprivate func preview_printsUpdates() -> Self {
+        var mutableSelf = self
+        mutableSelf.printsUpdates = true
+        return mutableSelf
     }
 
 }
@@ -69,20 +103,39 @@ private struct PreviewContent {
 
 
 #Preview(traits: .zeroSpacing, PreviewContent.layout) {
-    PreviewHeader()
+    PreviewHeader(flexibleSize: false)
+        .preview_printsUpdates()
+
     Divider()
+
     ConcentricRectangle(corners: .concentric(minimum: 12))
         .fill(.orange)
         .padding()
         .ignoresSafeArea()
+    // TODO: add toggle for flexible size
 }
 
 #Preview("SafeArea", traits: .zeroSpacing, PreviewContent.layout) {
 
     @Previewable @State var topSafeAreaInset: Double = 60.0
+
     let sliderRange: ClosedRange<Double> = 0.0...100.0
 
-    PreviewHeader()
+    // TODO: add toggle to disable this
+    Text("clear from device safe area")
+            .font(.caption)
+            .maxWidthFrame()
+            .padding(.bottom)
+            .padding(.bottom)
+            .background {
+                ConcentricRectangle(corners: .concentric(minimum: .fixed(HeaderFooterPreview<EmptyView>.minConcentricRoundedCornerRadius)))
+                    .fill(.orange.tertiary)
+                    .padding()
+                    .ignoresSafeArea()
+            }
+
+    PreviewHeader(flexibleSize: false)
+    .preview_printsUpdates()
     .safeAreaInset(edge: .top, spacing: 0) {
         let roundedHeight = topSafeAreaInset.rounded(.toNearestOrEven)
         Rectangle()
