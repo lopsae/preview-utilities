5676049260c7723559649e48684ccfb450872f99
Maic Lopez Saenz <maic@lopsae.com>
2025-12-30 14:42:12 -0800

    add a preview trait with iphone approximate size

---
+++ sources/miscelaneous/SizesPreviewTrait.swift
@@ -0,0 +1,28 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+import SwiftUI
+
+
+extension PreviewTrait where T == Preview.ViewTraits {
+
+    /// Returns a fixed layout preview trait with the approximate size , `400x800`, of the iPhone
+    /// Pro safe area.
+    ///
+    /// The actual reported size of the safe area in the iPhone 17 Pro simulator is `402x778`.
+    public static var iphoneSize: PreviewTrait {
+        return .fixedLayout(width: 400, height: 800)
+    }
+
+}
+
+
+// MARK: - Previews
+
+#Preview(traits: .iphoneSize) { // 402, 778
+    ClearRectangle()
+        .debugOutline(options: .size)
+}
