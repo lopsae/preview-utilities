a83a9750767e11a556b3dff6ec7b45590a093916
Maic Lopez Saenz <maic@lopsae.com>
2026-01-03 17:32:10 -0800

    support markdown and multiline preview captions

--- sources/views/PreviewCaption.swift
+++ sources/views/PreviewCaption.swift
@@ -11,20 +11,61 @@ import SwiftUI
 /// ``HeaderFooterContainerView``. Intended to provide additional detail along a preview.
 struct PreviewCaption: View {
 
-    let string: String
+    /// String for each text produced in the caption.
+    let textStrings: [String]
 
-    /// Creates a preview caption with a given sequence of strings.
+    /// Creates a preview caption with a given sequence of strings joined together.
     ///
-    /// All the strings are joined into a single string: if any whitespace is present at the edges
-    /// of two strings, these are joined as-is; if no whitespace is present between strings, a
-    /// single space is added between them.
+    /// All the strings are joined into a single string: any whitespace present at the edgesof two
+    /// strings is preserved; if no whitespace is present between two strings a single space is
+    /// added between them.
     init(_ strings: String...) {
         guard !strings.isEmpty else {
-            self.string = String()
+            self.textStrings = [String()]
             return
         }
 
-        let stringsWithSpacing: [String] = strings.enumerated().map { index, string in
+        let joinedString = Self.joinStrings(strings)
+        self.textStrings = [joinedString]
+    }
+
+
+    private init(textStrings: [String]) {
+        self.textStrings = textStrings
+    }
+
+
+    var body: some View {
+        VStack(spacing: 8 ) {
+            ForEach(textStrings.enumerated(), id: \.offset) { index, string in
+                let markdownString = (try? AttributedString(markdown: string)) ?? AttributedString("[markdown failed!] " + string)
+                Text(markdownString)
+                    .maxWidthFrame(alignment: .leading)
+            }
+        }
+        .padding()
+        .background {
+            Rectangle().fill(.gray.tertiary)
+                .roundedRectangleClip(cornerRadius: HeaderFooterContainerView.minimumConcentricRadius)
+        }
+        .padding(.horizontal)
+    }
+
+
+    /// Creates a new preview caption appending a paragraph with a given sequence of strings joined
+    /// together.
+    func paragraph(_ strings: String...) -> Self {
+        let joinedString = Self.joinStrings(strings)
+        return .init(textStrings: textStrings + [joinedString])
+    }
+
+
+    /// Joins all the strings in a given array into a single string selectively adding whitespace.
+    ///
+    /// Any whitespace present at the edges of two strings is preserved; if no whitespace is present
+    /// between two strings a single space is added between them.
+    private static func joinStrings(_ strings: [String]) -> String {
+        let stringsToJoin: [String] = strings.enumerated().map { index, string in
             let nextIndex = strings.index(after: index)
             guard strings.indices.contains(nextIndex) else {
                 // Last, no modification.
@@ -39,21 +80,7 @@ struct PreviewCaption: View {
                 return string + " "
             }
         }
-
-        print("String: >\(stringsWithSpacing.joined())<")
-        self.string = stringsWithSpacing.joined()
-    }
-
-
-    var body: some View {
-        Text(verbatim: string)
-            .maxWidthFrame(alignment: .leading)
-            .padding()
-            .background {
-                Rectangle().fill(.gray.tertiary)
-                    .roundedRectangleClip(cornerRadius: HeaderFooterContainerView.minimumConcentricRadius)
-            }
-            .padding(.horizontal)
+        return stringsToJoin.joined()
     }
 }
 
@@ -63,9 +90,10 @@ struct PreviewCaption: View {
 
 #Preview("Default", traits: .regularSpacing, .headerFooter, .iphoneSize) {
     PreviewCaption(
-        "Caption for a preview",
+        "**Caption** for a preview",
         "that can have text defined",
-        "in multiple lines."
+        "in _multiple lines_ with ",
+        "`Markdown support`."
     )
 
     Rectangle().fill(.red.tertiary)
@@ -75,9 +103,22 @@ struct PreviewCaption: View {
 
 #Preview("Spacing", traits:  .regularSpacing, .headerFooter, .iphoneSize) {
     PreviewCaption(
-        "Normal", "space", "inserted", "between", "parameters.",
-        "\n",
-        "And  ", "existing", "  space", "  is  ", "persisted."
+        "**Inserts", "space**", "between", "parameters.",
+        "\nNewlines are ignored.",
+        "Existing", "  space", "  is  ", "**persisted**."
+    )
+
+    Rectangle().fill(.red.tertiary)
+        .frame(square: 100)
+}
+
+#Preview("Paragraph", traits:  .regularSpacing, .headerFooter, .iphoneSize) {
+    PreviewCaption(
+        "**Paragraphs**"
+    ).paragraph(
+        "To display text in different **paragraphs**"
+    ).paragraph(
+        "Use the _paragraph function_."
     )
 
     Rectangle().fill(.red.tertiary)
