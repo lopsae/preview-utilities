c5fa27e423cffd72d57dec80a2e762c5f092a00e
Maic Lopez Saenz <maic@lopsae.com>
2026-01-05 15:58:45 -0800

    add edgeInsets.init(horizontal:vertical:)

--- sources/additions/EdgeInsets+Additions.swift
+++ sources/additions/EdgeInsets+Additions.swift
@@ -10,17 +10,30 @@ import SwiftUI
 extension EdgeInsets {
 
 
+    @inlinable
     public init(all value: CGFloat) {
         self.init(top: value, leading: value, bottom: value, trailing: value)
     }
 
 
-    @inlinable public static func all(_ value: CGFloat) -> Self {
+    @inlinable
+    public init(horizontal: CGFloat, vertical: CGFloat) {
+        self.init(
+            top:      vertical,
+            leading:  horizontal,
+            bottom:   vertical,
+            trailing: horizontal)
+    }
+
+
+    @inlinable
+    public static func all(_ value: CGFloat) -> Self {
         .init(top: value, leading: value, bottom: value, trailing: value)
     }
 
 
-    @inlinable public static func horizontal(_ value: CGFloat) -> Self {
+    @inlinable
+    public static func horizontal(_ value: CGFloat) -> Self {
         .init(
             top:      .zero,
             leading:  value,
@@ -29,7 +42,8 @@ extension EdgeInsets {
     }
 
 
-    @inlinable public static func vertical(_ value: CGFloat) -> Self {
+    @inlinable
+    public static func vertical(_ value: CGFloat) -> Self {
         .init(
             top:      value,
             leading:  .zero,
