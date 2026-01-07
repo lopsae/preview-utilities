499de0334f2cc4730e9594691e4b3da5826f2474
Maic Lopez Saenz <maic@lopsae.com>
2026-01-06 17:33:50 -0800

    add forgoten FormatStyle.arithmeticRoundedInteger

--- sources/additions/FormatStyle+Additions.swift
+++ sources/additions/FormatStyle+Additions.swift
@@ -9,11 +9,17 @@ import Foundation
 
 extension FormatStyle where Self == FloatingPointFormatStyle<Double> {
 
+    @inlinable
+    public static var arithmeticRoundedInteger: FloatingPointFormatStyle<Double> {
+        .number.rounded(rule: .toNearestOrEven, increment: 1)
+    }
+
+    @inlinable
     public static var roundedIntegerToNearestOrEven: FloatingPointFormatStyle<Double> {
         .number.rounded(rule: .toNearestOrEven, increment: 1)
     }
 
-
+    @inlinable
     public static func fractionLength(_ length: Int) -> FloatingPointFormatStyle<Double> {
         .number.precision(.fractionLength(length))
     }
