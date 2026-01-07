1d81cc7fb39c307722b1376eaf18654012dc148c
Maic Lopez Saenz <maic@lopsae.com>
2026-01-07 14:50:00 -0800

    add comparable.clamped(to PartialRangeFrom

--- sources/additions/Comparable+Additions.swift
+++ sources/additions/Comparable+Additions.swift
@@ -6,12 +6,24 @@
 
 extension Comparable {
 
-    /// Returns the value of `self` clamped to the given closed `range`.
+    /// Returns the value of `self` clamped to a given closed range.
     ///
     /// - Parameter range: Closed range to clamp `self` to.
     /// - Returns: The clamped value of `self`.
-    @inlinable public func clamped(to range: ClosedRange<Self>) -> Self {
+    @inlinable
+    public func clamped(to range: ClosedRange<Self>) -> Self {
         let clampedValue = Swift.max(range.lowerBound, Swift.min(self, range.upperBound))
+        return clampedValue
+    }
+
+
+    /// Returns the value of `self` clamped to a given partial range with a lower bound.
+    ///
+    /// - Parameter range: Partial range to clamp `self` to.
+    /// - Returns: The clamped value of `self`.
+    @inlinable
+    public func clamped(to range: PartialRangeFrom<Self>) -> Self {
+        let clampedValue = Swift.max(range.lowerBound, self)
         return clampedValue
     }
 
--- tests/Comparable+Additions+Tests.swift
+++ tests/Comparable+Additions+Tests.swift
@@ -11,25 +11,27 @@ import Testing
 
 struct ComparableAdditionTests {
 
+    @Test func clampClosedRange() async throws {
+        #expect(Int(1).clamped(to: 4...6) == 4)
+        #expect(Int(4).clamped(to: 4...6) == 4)
+        #expect(Int(5).clamped(to: 4...6) == 5)
+        #expect(Int(6).clamped(to: 4...6) == 6)
+        #expect(Int(9).clamped(to: 4...6) == 6)
 
-    func castedClamp<T: Comparable>(_ value: T, to closedRange: ClosedRange<T>) -> T {
-        value.clamped(to: closedRange)
+        // Single value range.
+        #expect(Int(3).clamped(to: 5...5) == 5)
+        #expect(Int(5).clamped(to: 5...5) == 5)
+        #expect(Int(7).clamped(to: 5...5) == 5)
+
+        // Causes: Fatal error: Range requires lowerBound <= upperBound
+        // #expect(Int(5).clamped(to: 7...3) == 5)
     }
 
 
-    @Test func clamp() async throws {
-        #expect(castedClamp(1, to: 4...6) == 4)
-        #expect(castedClamp(4, to: 4...6) == 4)
-        #expect(castedClamp(5, to: 4...6) == 5)
-        #expect(castedClamp(6, to: 4...6) == 6)
-        #expect(castedClamp(9, to: 4...6) == 6)
-
-        #expect(castedClamp(3, to: 5...5) == 5)
-        #expect(castedClamp(5, to: 5...5) == 5)
-        #expect(castedClamp(7, to: 5...5) == 5)
-
-        // Fatal error: Range requires lowerBound <= upperBound
-        // #expect(castedClamp(5, to: 7...3) == 5)
+    @Test func clampPartialRangeFrom() async throws {
+        #expect(Int(2).clamped(to: 5...) == 5)
+        #expect(Int(5).clamped(to: 5...) == 5)
+        #expect(Int(7).clamped(to: 5...) == 7)
     }
 
 }
