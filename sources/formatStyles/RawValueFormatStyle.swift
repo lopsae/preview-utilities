ecace0bc90f9331dec853d0ee4fc62b7facd4465
Maic Lopez Saenz <maic@lopsae.com>
2026-01-09 18:15:46 -0800

    add RawValueFormatStyle

---
+++ sources/formatStyles/RawValueFormatStyle.swift
@@ -0,0 +1,32 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+import Foundation
+
+
+/// A structure that converts a `RawRepresentable` to its raw value.
+struct RawValueFormatStyle<Value: RawRepresentable>: FormatStyle
+where Value.RawValue: StringProtocol {
+
+    public init() { }
+
+    public func format(_ value: Value) -> String { String(value.rawValue) }
+
+}
+
+
+extension FormatStyle {
+
+    /// Returns a format style that outputs the raw value of a `RawRepresentable`.
+    static func rawValue<Value: RawRepresentable>() -> RawValueFormatStyle<Value>
+    where
+        Value.RawValue: StringProtocol,
+        Self == RawValueFormatStyle<Value>
+    {
+        .init()
+    }
+
+}
