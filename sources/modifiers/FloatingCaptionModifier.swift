5502145d796e6e01451b59baed9e7edc9e47cc9d
Maic Lopez Saenz <maic@lopsae.com>
2026-01-12 00:19:30 -0800

    make a struct for FloatingCaptionModifier

--- sources/modifiers/FloatingCaptionModifier.swift
+++ sources/modifiers/FloatingCaptionModifier.swift
@@ -7,19 +7,22 @@
 import SwiftUI
 
 
-extension View {
+/// Experimental labeling for interactive preview elements, specially rectangles.
+struct FloatingCaptionModifier: ViewModifier {
 
-    /// Experimental labeling for interactive preview elements, specially rectangles.
-    func floatingCaption(_ key: LocalizedStringKey, _ options: PreviewCaptionOptions...) -> some View {
-        self.overlay {
+    let localizedKey: LocalizedStringKey
+    let traits: [Trait]
+
+    func body(content: Content) -> some View {
+        content.overlay {
             GeometryReader { geometry in
                 ZStack(alignment: .bottom) {
                     VStack(spacing: .zero) {
-                        Text(key)
+                        Text(localizedKey)
                             .foregroundStyle(.secondary)
                             .font(.caption)
                             .fixedSize()
-                        if options.contains(.height) {
+                        if traits.contains(.height) {
                             Text("height: \(geometry.size.height, format: .fractionLength(2))")
                                 .foregroundStyle(.secondary)
                                 .font(.caption.monospaced())
@@ -28,15 +31,44 @@ extension View {
                     } // VStack
                 } // ZStack
                 .frame(size: geometry.size, alignment: .center)
-                .border(.quaternary, width: options.contains(.border) ? 1 : .zero)
+                .border(.quaternary, width: traits.contains(.border) ? 1 : .zero)
             } // GeometryReader
-        } // Overlay
+        } // overlay
     }
 
 }
 
 
-enum PreviewCaptionOptions {
-    case border
-    case height
+extension FloatingCaptionModifier {
+
+    enum Trait {
+        case border
+        case height
+    }
+
+}
+
+
+extension View {
+
+    func floatingCaption(_ key: LocalizedStringKey, _ traits: FloatingCaptionModifier.Trait...) -> some View {
+        modifier(FloatingCaptionModifier(localizedKey: key, traits: traits))
+    }
+
+}
+
+
+// MARK: - Previews
+
+
+#Preview("Default", traits: .iPhoneProSizeLayout) {
+    Rectangle()
+        .fill(.purple.tertiary)
+        .frame(width: 40, height: 200)
+        .floatingCaption("Tall Rectangle", .height)
+
+    Rectangle()
+        .fill(.indigo.tertiary)
+        .frame(width: 200, height: 10)
+        .floatingCaption("Short Rectangle", .height, .border)
 }
