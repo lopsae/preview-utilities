eb9e56cabec8697c35691706f227b82296d24e38
Maic Lopez Saenz <maic@lopsae.com>
2026-01-12 16:31:48 -0800

    use FloatingAlignedContainer in floatingCaption
    and add support for floating alignments

--- sources/modifiers/FloatingCaptionModifier.swift
+++ sources/modifiers/FloatingCaptionModifier.swift
@@ -14,24 +14,27 @@ struct FloatingCaptionModifier: ViewModifier {
     let traits: [Trait]
 
     func body(content: Content) -> some View {
-        content.overlay {
+        content
+        .border(.quaternary, width: traits.containsCase(.border) ? 1 : .zero)
+        .overlay {
             GeometryReader { geometry in
-                ZStack(alignment: .bottom) {
-                    VStack(spacing: .zero) {
+                let alignment = traits.alignment ?? .inner(.center)
+                FloatingAlignedContainer(alignment: alignment) { alignment, textAlignment in
+                    VStack(alignment: alignment.horizontal, spacing: .zero) {
                         Text(localizedKey)
                             .foregroundStyle(.secondary)
                             .font(.caption)
-                            .fixedSize()
-                        if traits.contains(.height) {
+                            .multilineTextAlignment(textAlignment)
+
+                        if traits.containsCase(.height) {
                             Text("height: \(geometry.size.height, format: .fractionLength(2))")
                                 .foregroundStyle(.secondary)
                                 .font(.caption.monospaced())
-                                .fixedSize()
                         }
                     } // VStack
-                } // ZStack
-                .frame(size: geometry.size, alignment: .center)
-                .border(.quaternary, width: traits.contains(.border) ? 1 : .zero)
+                    .padding(2)
+                    .fixedSize()
+                } // FloatingAlignedContainer
             } // GeometryReader
         } // overlay
     }
@@ -41,9 +44,36 @@ struct FloatingCaptionModifier: ViewModifier {
 
 extension FloatingCaptionModifier {
 
-    enum Trait {
+    enum Trait: CaseIdentifiable {
         case border
         case height
+        case alignment(FloatingAlignment)
+
+        enum Case {
+            case border, height, alignment
+        }
+
+        var `case`: Case {
+            switch self {
+            case .border: .border
+            case .height: .height
+            case .alignment: .alignment
+            }
+        }
+
+    }
+
+}
+
+
+extension BidirectionalCollection where Element == FloatingCaptionModifier.Trait {
+
+    var alignment: FloatingAlignment? {
+        let caseInstance = lastCase(.alignment)
+        if case .alignment(let alignment) = caseInstance {
+            return alignment
+        }
+        return nil
     }
 
 }
@@ -56,6 +86,38 @@ extension View {
     }
 
 }
+
+
+// MARK: - Experimental CaseIdentifiable
+
+
+nonisolated
+protocol CaseIdentifiable {
+    associatedtype Case: Hashable
+    var `case`: Case { get }
+}
+
+
+extension Sequence where Element: CaseIdentifiable {
+
+    func containsCase(_ case: Element.Case) -> Bool {
+        contains { $0.case == `case` }
+    }
+
+    func firstCase(_ case: Element.Case) -> Element? {
+        first { $0.case == `case` }
+    }
+
+}
+
+extension BidirectionalCollection where Element: CaseIdentifiable {
+
+    func lastCase(_ case: Element.Case) -> Element? {
+        last { $0.case == `case` }
+    }
+
+}
+
 
 
 // MARK: - Previews
@@ -71,12 +133,26 @@ private struct PreviewContent {
 
 #Preview("Default", traits: PreviewContent.layout) {
     Rectangle()
-        .fill(.purple.tertiary)
+        .fill(.indigo.tertiary)
         .frame(width: 40, height: 200)
         .floatingCaption("Tall Rectangle", .height)
 
     Rectangle()
         .fill(.indigo.tertiary)
-        .frame(width: 200, height: 10)
+        .frame(width: 200, height: 15)
         .floatingCaption("Short Rectangle", .height, .border)
+}
+
+
+#Preview("Alignments", traits: PreviewContent.layout) {
+    Rectangle()
+    .fill(.indigo.tertiary)
+    .frame(square: 200)
+    .overlay {
+        ForEach(FloatingAlignment.allCases) { alignment in
+            ClearRectangle()
+            .floatingCaption("aligned\n\(alignment.abbreviatedName)", .alignment(alignment))
+        }
+    }
+
 }
