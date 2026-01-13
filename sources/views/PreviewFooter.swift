8774b88671356232a782f8cc492b1cccc270a2b4
Maic Lopez Saenz <maic@lopsae.com>
2026-01-13 14:09:55 -0800

    add SafeAreaPad convenience view

--- sources/views/PreviewFooter.swift
+++ sources/views/PreviewFooter.swift
@@ -177,11 +177,38 @@ private struct PreviewContent {
     }
 
     if !useDeviceSafeArea {
-        Divider()
+        SafeAreaPad(topDivider: true)
+    }
+
+}
+
+
+struct SafeAreaPad<S: ShapeStyle>: View {
+
+    let topDivider: Bool
+    let bottomDivider: Bool
+    let backgroundFill: S
+
+
+    init(topDivider: Bool = false, bottomDivider: Bool = false, fill: S = .orange.tertiary) {
+        self.topDivider = topDivider
+        self.bottomDivider = bottomDivider
+        self.backgroundFill = fill
+    }
+
+    var body: some View {
+        if (topDivider) {
+            Divider()
+        }
+
         Text("clear from device safe area")
-        .font(.caption)
-        .maxWidthFrame()
-        .concentricSafeAreaBackground(fill: .orange.tertiary)
+            .font(.caption)
+            .maxWidthFrame()
+            .concentricSafeAreaBackground(fill: backgroundFill)
+
+        if (bottomDivider) {
+            Divider()
+        }
     }
 
 }
