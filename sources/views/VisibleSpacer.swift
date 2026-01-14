3e165c3eeb61c53f4e713f3b337cd9e9d1b0c961
Maic Lopez Saenz <maic@lopsae.com>
2026-01-14 00:00:19 -0800

    add VisibleSpacer previews

--- sources/views/VisibleSpacer.swift
+++ sources/views/VisibleSpacer.swift
@@ -19,3 +19,40 @@ struct VisibleSpacer: View {
     }
 
 }
+
+
+// MARK: - Previews
+
+
+@MainActor
+private struct PreviewContent {
+
+    static let layout: PreviewTrait<Preview.ViewTraits> = .iPhoneProSizeLayout
+
+}
+
+
+#Preview("Default", traits: .headerFooter(.showDividers), PreviewContent.layout) {
+    VisibleSpacer()
+}
+
+
+#Preview("Sizing", traits: .zeroSpacing, .headerFooter(.showDividers), PreviewContent.layout) {
+    @Previewable @State var fixedHeight: Double = 100
+
+    Slider.captioned(
+        "Fixed Content Height",
+        value: $fixedHeight,
+        in: 0...500,
+        currentValueFormat: .fractionLength(2),
+        boundsValueFormat: .arithmeticRoundedInteger)
+    .padding()
+
+    Rectangle()
+        .fill(.mint.tertiary)
+        .frame(width: 150, height: fixedHeight)
+        .floatingCaption("Fixed Content", .border, .height)
+
+    VisibleSpacer()
+}
+
