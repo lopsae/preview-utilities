4c7ecedd85236009236bacea81ad62e28a7ff2ff
Maic Lopez Saenz <maic@lopsae.com>
2026-01-14 13:47:15 -0800

    add Alignment additions

---
+++ sources/additions/Alignment+Additions.swift
@@ -0,0 +1,19 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+import SwiftUI
+
+
+extension VerticalAlignment {
+    nonisolated
+    static var verticalCenter: Self { .center }
+}
+
+
+extension HorizontalAlignment {
+    nonisolated
+    static var horizontalCenter: Self { .center }
+}
