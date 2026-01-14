4072489b3faf6e61c955bcf4a09702bef263023a
Maic Lopez Saenz <maic@lopsae.com>
2026-01-14 13:19:24 -0800

    add an enum with the default padding values

---
+++ sources/miscelaneous/DefaultPaddings.swift
@@ -0,0 +1,42 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+import SwiftUI
+
+
+enum DefaultPaddings {
+
+    static let horizontal: CGFloat = 16
+    static let vertical: CGFloat = 16
+
+}
+
+
+#Preview("Paddings") {
+    Rectangle()
+    .fill(.cyan.gradient.secondary)
+    .frame(square: 200)
+    .overlay {
+        GeometryReader { outerGeometry in
+            Rectangle()
+            .fill(.brown.gradient.tertiary)
+            .overlay {
+                GeometryReader { innerGeometry in
+                    let horizontalPadding = (outerGeometry.size.width - innerGeometry.size.width) / 2
+                    let verticalPadding = (outerGeometry.size.height - innerGeometry.size.height) / 2
+                    ClearRectangle()
+                    .floatingCaption(
+                        "Vertical: `\(verticalPadding, format: .fractionLength(2))`",
+                        .alignment(.innerTop))
+                    .floatingCaption(
+                        "Horizontal:\n`\(horizontalPadding, format: .fractionLength(2))`",
+                        .alignment(.innerTrailing))
+                }
+            }
+            .padding()
+        }
+    }
+}
