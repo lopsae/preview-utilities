308f4add47d59ca1a6fb945232796055d07f27b5
Maic Lopez Saenz <maic@lopsae.com>
2026-01-15 00:04:07 -0800

    add preview for CaptionRectangle

--- sources/views/CaptionRectangle.swift
+++ sources/views/CaptionRectangle.swift
@@ -19,7 +19,7 @@ struct CaptionRectangle<Fill: ShapeStyle, Stroke: ShapeStyle>: View {
 
     init(
         _ localizationKey: LocalizedStringKey,
-        fill: Fill,
+        fill: Fill = .gray.gradient.tertiary,
         stroke: Stroke = .tertiary,
         width: CGFloat? = nil,
         height: CGFloat? = nil,
@@ -37,8 +37,32 @@ struct CaptionRectangle<Fill: ShapeStyle, Stroke: ShapeStyle>: View {
     var body: some View {
         RoundedRectangle(cornerRadius: Defaults.padding / 3)
             .fill(fill)
-            .stroke(.tertiary)
+            .stroke(stroke)
             .frame(width: width, height: height)
             .floatingCaption(localizationKey, traits: traits)
     }
+}
+
+
+// MARK: - Preview
+
+
+#Preview("Default", traits: .iPhoneProSizeForcedLayout) {
+    CaptionRectangle(
+        "Caption Rectangle",
+        width: 120, height: 120,
+        traits: .height)
+
+    CaptionRectangle(
+        "Caption Rectangle",
+        fill: .cyan.gradient.tertiary,
+        width: 120, height: 120,
+        traits: .width)
+
+    CaptionRectangle(
+        "Caption Rectangle",
+        fill: .mint.gradient.tertiary,
+        stroke: .clear,
+        width: 120, height: 120,
+        traits: .height)
 }
