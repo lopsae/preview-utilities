046c7e21641cec5015366c463bd56f500db08162
Maic Lopez Saenz <maic@lopsae.com>
2026-01-20 23:25:39 -0800

    publicize PrintOnce

--- sources/miscelaneous/PrintOnce.swift
+++ sources/miscelaneous/PrintOnce.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 /// Experimental observable object to print a log message during the first request of views.
 ///
-/// Call `view` within body to print `mesage` the first time it is called. This is currently the
-/// best know way to print a message consistently at the begining of a preview.
+/// Call `print()` within body to print `mesage` the first time it is called. This is currently
+/// the best know way to print a message consistently at the begining of a preview.
 @Observable
-class PrintOnce {
+public class PrintOnce {
 
     let message: String
     private(set) var hasPrinted: Bool = false
+
 
     init(_ message: String) {
         self.message = message
     }
 
+
     /// Prints `message` the only the first time it is called, always returns `EmptyView`.
-    func print() -> EmptyView {
+    public func print() -> EmptyView {
         if !hasPrinted {
             hasPrinted = true
             Swift.print(message)
@@ -30,14 +32,8 @@ class PrintOnce {
         return EmptyView()
     }
 
-    // TODO: remove deprecations after other projects update, before release.
-    @available(*, deprecated, renamed: "print()")
-    var view: EmptyView {
-        print()
-    }
 
-
-    static var previewStarted: PrintOnce {
+    public static var previewStarted: PrintOnce {
         .init("✴️ Preview started")
     }
 
