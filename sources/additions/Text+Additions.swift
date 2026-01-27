8f4b197da0cc1ac1634eec175ba296b433717230
Maic Lopez Saenz <maic@lopsae.com>
2026-01-27 11:23:49 -0800

    add Text.caption addition

---
+++ sources/additions/Text+Additions.swift
@@ -0,0 +1,37 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+import SwiftUI
+
+
+extension Text {
+
+    //TODO: look for `(.caption` to replace.
+    public static func caption(_ key: LocalizedStringKey) -> Self {
+        Self(key).font(.caption)
+    }
+
+}
+
+
+// MARK: - PreviewContent
+
+
+@MainActor
+private struct PreviewContent {
+
+    static let layout: PreviewTrait<Preview.ViewTraits> = .iPhoneProSizeLayout
+
+}
+
+
+// MARK: - Previews
+
+
+#Preview("Default", traits: .headerFooter, PreviewContent.layout) {
+    Text("Regular `Text`")
+    Text.caption("Captioned `Text.caption`")
+}
