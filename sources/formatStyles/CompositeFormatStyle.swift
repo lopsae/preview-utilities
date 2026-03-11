d723284a85fbeedd87c77a243ea12ee0c3abf34a
Maic Lopez Saenz <maic@lopsae.com>
2026-03-11 16:00:20 -0700

    add preview to CompositeFormatStyle

--- sources/formatStyles/CompositeFormatStyle.swift
+++ sources/formatStyles/CompositeFormatStyle.swift
@@ -4,7 +4,7 @@
 //
 
 
-import Foundation
+import SwiftUI
 
 
 /// A Structure that joins two format styles.
@@ -21,3 +21,31 @@ where InputFormat.FormatOutput == OutputFormat.FormatInput {
     }
 
 }
+
+
+// MARK: - PreviewContent
+
+
+@MainActor
+private struct PreviewContent {
+
+    static let layout: PreviewTrait<Preview.ViewTraits> = .iPhoneProSizeLayout
+
+    nonisolated
+    struct Dummy: CustomStringConvertible {
+        var description: String { "description" }
+    }
+
+}
+
+
+// MARK: - Previews
+
+
+#Preview("Default", traits: .fixedHeader, PreviewContent.layout) {
+    @Previewable let dummy = PreviewContent.Dummy()
+    Text("Identity + Capitalized: `\("lorem ipsum", format: CompositeFormatStyle(input: .identity, output: .capitalized))`")
+    Text("First Letter + Capitalized: `\("lorem ipsum", format: CompositeFormatStyle(input: .firstCharacter, output: .capitalized))`")
+    Text("Description + Capitalized: `\(dummy, format: CompositeFormatStyle(input: .description(), output: .capitalized))`")
+}
+
