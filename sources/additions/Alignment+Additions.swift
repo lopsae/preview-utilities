d0bb5c5cce859489267bbf18c9dd16663a3477a0
Maic Lopez Saenz <maic@lopsae.com>
2026-03-11 11:32:35 -0700

    move Horizontal/VerticalAlignmentEnum to their own file

--- sources/additions/Alignment+Additions.swift
+++ sources/additions/Alignment+Additions.swift
@@ -17,3 +17,56 @@ extension HorizontalAlignment {
     nonisolated
     static var horizontalCenter: Self { .center }
 }
+
+
+/// Enumeration of the alignment instances available in ``SwiftUICore/HorizontalAlignment``.
+///
+/// Allows previews and other consumers to list the alignment options available.
+enum HorizontalAlignmentEnum: String, SelfIdentifiable, CaseIterable {
+
+    case leading, center, traling
+
+    /// Returns the corresponding ``SwiftUI/HorizontalAlignment``.
+    var alignment: HorizontalAlignment {
+        switch self {
+        case .leading: .leading
+        case .center:  .center
+        case .traling: .trailing
+        }
+    }
+
+    // TODO: could use display property?
+    var displayName: String { rawValue }
+
+}
+
+
+/// Enumeration of the alignment instances available in ``SwiftUI/VerticalAlignment``.
+///
+/// Allows previews and other consumers to list the alignment options available.
+enum VerticalAlignmentEnum: String, SelfIdentifiable, CaseIterable {
+
+    case top, center, bottom
+    case firstTextBaseline, lastTextBaseline
+
+    /// Returns the corresponding ``SwiftUI/VerticalAlignment``.
+    var alignment: SwiftUI.VerticalAlignment {
+        switch self {
+        case .top:    .top
+        case .center: .center
+        case .bottom: .bottom
+        case .firstTextBaseline: .firstTextBaseline
+        case .lastTextBaseline:  .lastTextBaseline
+        }
+    }
+
+    // TODO: could use display property?
+    var displayName: String {
+        switch self {
+        case .top, .center, .bottom: rawValue
+        case .firstTextBaseline: "first text baseline"
+        case .lastTextBaseline:  "last text baseline"
+        }
+    }
+
+}
--- sources/views/ConstrainedFill.swift
+++ sources/views/ConstrainedFill.swift
@@ -98,60 +98,6 @@ private struct PreviewContent {
 }
 
 
-// TODO: Move to own file.
-/// Enumeration of the alignment instances available in ``SwiftUICore/HorizontalAlignment``.
-///
-/// Allows previews and other consumers to list the alignment options available.
-enum HorizontalAlignmentEnum: String, SelfIdentifiable, CaseIterable {
-
-    case leading, center, traling
-
-    /// Returns the corresponding ``SwiftUI/HorizontalAlignment``.
-    var alignment: HorizontalAlignment {
-        switch self {
-        case .leading: .leading
-        case .center:  .center
-        case .traling: .trailing
-        }
-    }
-
-    // TODO: could use display property?
-    var displayName: String { rawValue }
-
-}
-
-// TODO: Move to own file.
-/// Enumeration of the alignment instances available in ``SwiftUI/VerticalAlignment``.
-///
-/// Allows previews and other consumers to list the alignment options available.
-enum VerticalAlignmentEnum: String, SelfIdentifiable, CaseIterable {
-
-    case top, center, bottom
-    case firstTextBaseline, lastTextBaseline
-
-    /// Returns the corresponding ``SwiftUI/VerticalAlignment``.
-    var alignment: SwiftUI.VerticalAlignment {
-        switch self {
-        case .top:    .top
-        case .center: .center
-        case .bottom: .bottom
-        case .firstTextBaseline: .firstTextBaseline
-        case .lastTextBaseline:  .lastTextBaseline
-        }
-    }
-
-    // TODO: could use display property?
-    var displayName: String {
-        switch self {
-        case .top, .center, .bottom: rawValue
-        case .firstTextBaseline: "first text baseline"
-        case .lastTextBaseline:  "last text baseline"
-        }
-    }
-
-}
-
-
 #Preview("Alignments", traits: .fixedHeaderFooter, PreviewContent.layout) {
     @Previewable @State var horizontalAlignment: HorizontalAlignmentEnum = .center
     @Previewable @State var verticalAlignment: VerticalAlignmentEnum = .center
