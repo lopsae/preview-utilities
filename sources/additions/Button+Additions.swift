a3147110e20763b37906c6f55c21e85d7f66a1dc
Maic Lopez Saenz <maic@lopsae.com>
2026-03-18 12:46:40 -0700

    add sizedSystemImage Button initializer

--- sources/additions/Button+Additions.swift
+++ sources/additions/Button+Additions.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 extension Button {
 
+    init(
+        _ titleKey: LocalizedStringKey,
+        sizedSystemImage systemImage: String,
+        action: @escaping () -> Void
+    )
+        where Label == SwiftUI.Label<Text, HiddenOverlay<Text, Image>>
+    {
+        self.init(action: action) {
+            SwiftUI.Label {
+                Text(titleKey)
+            } icon: {
+                HiddenOverlay {
+                    Text("M")
+                } content: {
+                    Image(systemName: systemImage)
+                }
+            }
+        }
+    }
 
+}
+
+
+struct HiddenOverlay<Parent: View, Content: View>: View {
+    @ViewBuilder let parent: Parent
+    @ViewBuilder let content: Content
+    var body: some View {
+        parent
+            .hidden()
+            .accessibilityHidden(true)
+            .overlay(alignment: .center) {
+                content
+            }
+    }
 }
 
 
@@ -29,6 +62,8 @@ private struct PreviewContent {
 
 #Preview("Default", traits: .headerFooter, PreviewContent.layout) {
     @Previewable @State var circleToggle: Bool = false
+    @Previewable @State var guidepointToggle: Bool = false
+
     PreviewCaption("""
         Buttons using the `.iconOnly` style will change its size depending on the image being used.
         """)
@@ -42,8 +77,44 @@ private struct PreviewContent {
             .buttonStyle(.borderedProminent)
             .labelStyle(.iconOnly)
 
+        Button("Vertical", systemImage: guidepointToggle ? "guidepoint.vertical" : "guidepoint.horizontal", action: { guidepointToggle.toggle() })
+            .buttonStyle(.borderedProminent)
+            .labelStyle(.iconOnly)
+
         Button("Plus", systemImage: "plus", action: {})
             .buttonStyle(.borderedProminent)
             .labelStyle(.iconOnly)
     }
+
+
+    PreviewCaption("""
+        Buttons using the `sizedSystemImage` initializer have a constrained size, irregardless
+        of the image used.
+        """)
+
+    HStack {
+        Button("Minus", sizedSystemImage: "minus", action: {})
+            .buttonStyle(.borderedProminent)
+            .labelStyle(.iconOnly)
+
+        Button("Circle", sizedSystemImage: circleToggle ? "circle.fill" : "circle", action: { circleToggle.toggle() })
+            .buttonStyle(.borderedProminent)
+            .labelStyle(.iconOnly)
+
+        Button("Vertical", sizedSystemImage: guidepointToggle ? "guidepoint.vertical" : "guidepoint.horizontal", action: { guidepointToggle.toggle() })
+            .buttonStyle(.borderedProminent)
+            .labelStyle(.iconOnly)
+
+        Button("Plus", sizedSystemImage: "plus", action: {})
+            .buttonStyle(.borderedProminent)
+            .labelStyle(.iconOnly)
+    }
+
+    // TODO: put in separate preview
+    Text.caption("Constrained")
+    Button("Label", sizedSystemImage: "guidepoint.horizontal", action: {})
+        .buttonStyle(.borderedProminent)
+    Button("Label", systemImage: "guidepoint.horizontal", action: {})
+        .buttonStyle(.borderedProminent)
+    Text.caption("Regular")
 }
