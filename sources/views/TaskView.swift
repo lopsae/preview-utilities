1336714891922b30df880659ef7e1f4a7318293f
Maic Lopez Saenz <maic@lopsae.com>
2026-03-22 11:20:53 -0700

    provide awaitContent to TaskView

--- sources/views/TaskView.swift
+++ sources/views/TaskView.swift
@@ -14,33 +14,37 @@ import SwiftUI
 /// when the task produces a result.
 ///
 /// If the view is removed, the task is cancelled.
-struct TaskView<Result, Content>: View
-where Result: Sendable, Content: View
+struct TaskView<Result, AwaitContent, ResultContent>: View
+where Result: Sendable, AwaitContent: View, ResultContent: View
 {
     let taskBody: () async -> Result
-    let content: (Result) -> Content
+    let awaitContent: () -> AwaitContent
+    let resultContent: (Result) -> ResultContent
 
     @State var result: Result?
 
     init(
         // TODO: since this is a view, should this be isolated to main actor?
         @_inheritActorContext taskBody: @Sendable @escaping @isolated(any) () async -> Result,
-        @ViewBuilder content: @escaping (Result) -> Content
+        @ViewBuilder await awaitContent: @escaping () -> AwaitContent,
+        @ViewBuilder result resultContent: @escaping (Result) -> ResultContent
     ) {
         self.taskBody = taskBody
-        self.content = content
+        self.awaitContent = awaitContent
+        self.resultContent = resultContent
     }
 
     var body: some View {
         Group {
             if let result {
-                content(result)
+                resultContent(result)
             } else {
                 // A view is needed here for a view to exist in some cases to hold the task.
                 // For example: inside stacks, `EmptyView` are removed, and the task never runs.
                 // TODO: this can be the default, but another view could be provided instead?
                 // TODO: add a preview showing that providing an EmptyView in a stack prevents the task from running.
-                ClearRectangle(size: .zero)
+//                ClearRectangle(size: .zero)
+                awaitContent()
             }
         }
         .task {
@@ -73,9 +77,10 @@ private struct PreviewContent {
                 TaskView {
                     let generator = ConcurrentImageGenerator(size: imageSize, sleepRange: .seconds(0.5) ... .seconds(1.5))
                     // TODO: use function that produces uiImage.
-                    print("in task")
                     return try! await generator.generateImage(with: "Task Image").image
-                } content: { image in
+                } await: {
+                    ClearRectangle(size: .zero)
+                } result: { image in
                     image
                 }
             }
@@ -98,9 +103,38 @@ private struct PreviewContent {
             // TODO: use function that produces uiImage.
             print("in task")
             return try! await generator.generateImage(with: "Task Image").image
-        } content: { image in
+        } await: {
+            ClearRectangle(size: .zero)
+        } result: { image in
             image
         }
     }
+}
 
+
+// TODO: use padding spacing.
+#Preview("EmptyView", traits: .headerFooter, PreviewContent.layout) {
+    PreviewCaption("""
+        Some views like `ZStack` do a special treatment of `EmptyView`s removing them from the view
+        hierachy.
+        """
+    ).paragraph("""
+        In these cases the `.task` is also removed and never executes.
+        """)
+
+    let imageSize: CGSize = .square(of: 150)
+    ZStack {
+        CaptionRectangle("Placeholder", color: .gray, size: imageSize)
+
+        TaskView {
+            let generator = ConcurrentImageGenerator(size: imageSize, sleepRange: .seconds(0.5) ... .seconds(1.5))
+            // TODO: use function that produces uiImage.
+            print("in task")
+            return try! await generator.generateImage(with: "Task Image").image
+        } await: {
+            EmptyView()
+        } result: { image in
+            image
+        }
+    }
 }
