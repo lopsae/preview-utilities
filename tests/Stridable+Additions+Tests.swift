0c9e83459e4a7a7ab1f0c839383125a446880ca9
Maic Lopez Saenz <maic@lopsae.com>
2026-03-23 14:14:17 -0700

    fix tiny testing todo

--- tests/Stridable+Additions+Tests.swift
+++ tests/Stridable+Additions+Tests.swift
@@ -11,30 +11,21 @@ import Testing
 
 struct StridableAdditionTests {
 
-
-    func castedClamp<T: Strideable>(_ value: T, to range: Range<T>) -> T?
-    where T.Stride == Int {
-        value.clamped(to: range)
-    }
-
-
     @Test func clamp() async throws {
-        // TODO: remove castedClamp function.
         #expect(1.clamped(to: 4..<7) == 4)
-        #expect(castedClamp(1, to: 4..<7) == 4)
-        #expect(castedClamp(4, to: 4..<7) == 4)
-        #expect(castedClamp(5, to: 4..<7) == 5)
-        #expect(castedClamp(6, to: 4..<7) == 6)
-        #expect(castedClamp(7, to: 4..<7) == 6)
-        #expect(castedClamp(9, to: 4..<7) == 6)
+        #expect(4.clamped(to: 4..<7) == 4)
+        #expect(5.clamped(to: 4..<7) == 5)
+        #expect(6.clamped(to: 4..<7) == 6)
+        #expect(7.clamped(to: 4..<7) == 6)
+        #expect(9.clamped(to: 4..<7) == 6)
 
         // With empty range
-        #expect(castedClamp(3, to: 5..<5) == nil)
-        #expect(castedClamp(5, to: 5..<5) == nil)
-        #expect(castedClamp(7, to: 5..<5) == nil)
+        #expect(3.clamped(to: 5..<5) == nil)
+        #expect(5.clamped(to: 5..<5) == nil)
+        #expect(7.clamped(to: 5..<5) == nil)
 
         // Fatal error: Range requires lowerBound <= upperBound
-        // #expect(castedClamp(5, to: 7..<3) == 5)
+        // #expect(5.clamped(to: 7..<3) == 5)
     }
 
 }
