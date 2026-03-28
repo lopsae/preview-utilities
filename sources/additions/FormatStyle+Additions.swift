7b344e6565d895cef62ac7954f28b63512f860ea
Maic Lopez Saenz <maic@lopsae.com>
2026-03-27 22:18:43 -0700

    nonisolate FormatStyle additions

--- sources/additions/FormatStyle+Additions.swift
+++ sources/additions/FormatStyle+Additions.swift
@@ -9,12 +9,12 @@ import Foundation
 
 extension FormatStyle where Self == FloatingPointFormatStyle<Double> {
 
-    @inlinable
+    @inlinable nonisolated
     public static var arithmeticRoundedInteger: FloatingPointFormatStyle<Double> {
         .number.rounded(rule: .toNearestOrEven, increment: 1)
     }
 
-    @inlinable
+    @inlinable nonisolated
     public static func fractionLength(_ length: Int) -> FloatingPointFormatStyle<Double> {
         .number.precision(.fractionLength(length))
     }
