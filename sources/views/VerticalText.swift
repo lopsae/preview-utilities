ef4fe6daa0de57eda808957ff80d949f374d462f
Maic Lopez Saenz <maic@lopsae.com>
2026-03-27 22:19:45 -0700

    add DummyZStack to test layouts

--- sources/views/VerticalText.swift
+++ sources/views/VerticalText.swift
@@ -109,6 +109,76 @@ private struct TransposeLayout: Layout {
 }
 
 
+extension ProposedViewSize {
+
+    nonisolated
+    var debugSizeString: String {
+        let widthString = width?.formatted(.fractionLength(1)) ?? "nil"
+        let heightString = height?.formatted(.fractionLength(1)) ?? "nil"
+        return "(w:\(widthString), h:\(heightString))"
+    }
+
+}
+
+
+nonisolated
+private struct DummyZStack: Layout {
+
+    func sizeThatFits(
+        proposal: ProposedViewSize,
+        subviews: Subviews,
+        cache: inout ()
+    ) -> CGSize {
+        print("❎ sizeThatFits:")
+        print(" ┗ proposal: \(proposal.debugSizeString) ")
+
+        var containerSize: CGSize = .zero
+        for subview in subviews {
+            let size = subview.sizeThatFits(proposal)
+            containerSize.envelop(size)
+        }
+        return containerSize
+    }
+
+
+    func placeSubviews(
+        in bounds: CGRect,
+        proposal: ProposedViewSize,
+        subviews: Subviews,
+        cache: inout ()
+    ) {
+        print("🔡 placeSubviews:")
+        print(" ┗ bounds: +:\(bounds.origin) ◻:\(bounds.size) ")
+        print(" ┗ proposal: ◻\(proposal.debugSizeString) ")
+
+        for subview in subviews {
+            subview.place(
+                at: bounds.center,
+                anchor: .center,
+                proposal: proposal
+            )
+        }
+    }
+
+}
+
+
+#Preview("DummyZStack", traits: .headerFooter, PreviewContent.layout) {
+    @Previewable let printOnce: PrintOnce = .previewStarted
+
+    printOnce.print()
+    DummyZStack {
+        CaptionRectangle(
+            "Tall", color: .yellow, size: .init(width: 200, height: 100),
+            traits: .alignment(.topLeading))
+        CaptionRectangle(
+            "Wide", color: .brown, size: .init(width: 100, height: 200),
+            traits: .alignment(.topTrailing))
+    }
+    .debugOverlay(.size, .infoAlignment(.outerBottom))
+}
+
+
 // MARK: - PreviewContent
 
 
@@ -124,6 +194,27 @@ private struct PreviewContent {
 
 
 #Preview("Default", traits: .fixedHeader, PreviewContent.layout) {
+    HStack {
+        VerticalText("Upwards Label", direction: .upwards)
+            .border(.tertiary)
+
+        Rectangle()
+            .fill(.cyan.gradient.tertiary)
+            .frame(width: 200, height: 100)
+    }
+
+    HStack {
+        VerticalText("Downwards Label", direction: .downwards)
+            .border(.tertiary)
+
+        Rectangle()
+            .fill(.mint.gradient.tertiary)
+            .frame(width: 200, height: 100)
+    }
+}
+
+
+#Preview("TransposeLayout", traits: .fixedHeader, PreviewContent.layout) {
     HStack {
         VerticalText("Upwards Label", direction: .upwards)
             .border(.tertiary)
