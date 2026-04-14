8418d08ce0184b85395e2b8105cdcf6cf5bd399c
Maic Lopez Saenz <maic@lopsae.com>
2026-04-14 16:19:53 -0700

    add appendInterpolation(_:format:)

--- sources/additions/DefaultStringInterpolation+Additions.swift
+++ sources/additions/DefaultStringInterpolation+Additions.swift
@@ -4,14 +4,31 @@
 //
 
 
+import Foundation
+
+
 extension DefaultStringInterpolation {
 
+    @inlinable public nonisolated
     mutating func appendInterpolation<T>(nilDefault value: T?) {
         appendInterpolation(value, default: "nil")
     }
 
+    @inlinable public nonisolated
     mutating func appendInterpolation<T>(emptyDefault value: T?) {
         appendInterpolation(value, default: String())
+    }
+
+
+    /// Appends the formatted representation of a nonstring type supported by a corresponding
+    /// format style.
+    ///
+    /// Based on `LocalizedStringKey.StringInterpolation/appendInterpolation(_:format:)`.
+    @inlinable public nonisolated
+    mutating func appendInterpolation<Format>(_ value: Format.FormatInput, format: Format)
+    where Format: FormatStyle, Format.FormatOutput == String {
+        let formattedValue = format.format(value)
+        appendInterpolation(formattedValue)
     }
 
 }
