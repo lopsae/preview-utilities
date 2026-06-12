70203da3ed0f7109eda9e10b1c8840112a010504
Maic Lopez Saenz <maic@lopsae.com>
2026-06-12 14:48:00 -0700

    add tests

---
+++ tests/formatStyles/CapitalizeFormatStyle+Tests.swift
@@ -0,0 +1,54 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+import PreviewUtilities
+
+import Testing
+
+
+struct CapitalizeFormatStyleTests {
+
+    nonisolated struct Sample: Equatable, FormatStyleFormattable {
+        let label = "lorem"
+    }
+
+    @Test func extensions() {
+        #expect("black quartz".formatted(.capitalize) == "Black Quartz")
+        #expect("black quartz".formatted(.capitalize(.firstWord)) == "Black")
+        #expect(Sample().formatted(.capitalize(property: \.label)) == "Lorem")
+    }
+
+    @Test func words() {
+        let style = CapitalizeFormatStyle()
+        #expect(style.format("black quartz") == "Black Quartz")
+    }
+
+    @Test func emptyString() {
+        let style = CapitalizeFormatStyle()
+        #expect(style.format("") == "")
+    }
+
+    @Test func singleWord() {
+        let style = CapitalizeFormatStyle()
+        #expect(style.format("hello") == "Hello")
+    }
+
+    @Test func alreadyCapitalized() {
+        let style = CapitalizeFormatStyle()
+        #expect(style.format("Black Quartz") == "Black Quartz")
+    }
+
+    @Test func allUppercase() {
+        let style = CapitalizeFormatStyle()
+        #expect(style.format("BLACK QUARTZ") == "Black Quartz")
+    }
+
+    @Test func singleCharacter() {
+        let style = CapitalizeFormatStyle()
+        #expect(style.format("a") == "A")
+    }
+
+}
