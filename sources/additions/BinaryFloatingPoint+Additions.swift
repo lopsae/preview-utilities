aeb3a315374257cb5ce5726b7208f532713b72f3
Maic Lopez Saenz <maic@lopsae.com>
2026-06-29 23:27:33 -0700

    add floating point .one and .half

---
+++ sources/additions/BinaryFloatingPoint+Additions.swift
@@ -0,0 +1,12 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+extension BinaryFloatingPoint {
+
+    static var one: Self { Self(1) }
+    static var half: Self { Self(0.5) }
+
+}
