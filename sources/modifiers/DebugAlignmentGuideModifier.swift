0db2876013ef74c751f37a55f6721fb12000ae9a
Maic Lopez Saenz <maic@lopsae.com>
2026-06-30 20:19:07 -0700

    add DebugVerticalAlignmentGuideModifier

--- sources/modifiers/DebugAlignmentGuideModifier.swift
+++ sources/modifiers/DebugAlignmentGuideModifier.swift
@@ -15,8 +15,24 @@ struct DebugHorizontalAlignmentGuideModifier: ViewModifier {
         let alignment = Alignment(horizontal: horizontalAlignment, vertical: .center)
         content.overlay(alignment: alignment) {
             Rectangle()
-                .fill(.red.secondary)
-                .frame(width: 2)
+            .fill(.red.secondary)
+            .frame(width: 2)
+        }
+    }
+
+}
+
+
+struct DebugVerticalAlignmentGuideModifier: ViewModifier {
+
+    let verticalAlignment: VerticalAlignment
+
+    func body(content: Content) -> some View {
+        let alignment = Alignment(horizontal: .center, vertical: verticalAlignment)
+        content.overlay(alignment: alignment) {
+            Rectangle()
+            .fill(.red.secondary)
+            .frame(height: 2)
         }
     }
 
@@ -30,6 +46,11 @@ extension View {
 
     public func debugAlignmentGuide(_ horizontalAlignment: HorizontalAlignment) -> some View {
         return modifier(DebugHorizontalAlignmentGuideModifier(horizontalAlignment: horizontalAlignment))
+    }
+
+
+    public func debugAlignmentGuide(_ verticalAlignment: VerticalAlignment) -> some View {
+        return modifier(DebugVerticalAlignmentGuideModifier(verticalAlignment: verticalAlignment))
     }
 
 }
@@ -49,12 +70,35 @@ private struct PreviewContent {
 // MARK: - Previews
 
 
-#Preview("Default", traits: .headerFooter, PreviewContent.layout) {
+#Preview("Horizontal", traits: .headerFooter, PreviewContent.layout) {
     Text("Ag")
     .font(.title.pointSize(100))
     .debugAlignmentGuide(.leading)
-    .debugAlignmentGuide(.center)
+    .debugAlignmentGuide(.horizontalCenter)
     .debugAlignmentGuide(.trailing)
-    .border(.green.quinary, width: 4)
+    .border(.green.tertiary, width: 8)
+}
+
+
+#Preview("Vertical", traits: .fixedHeader, PreviewContent.layout) {
+    Text("Ag")
+    .font(.title.pointSize(100))
+    .debugAlignmentGuide(.top)
+    .debugAlignmentGuide(.firstTextBaseline)
+    .debugAlignmentGuide(.verticalCenter)
+    .debugAlignmentGuide(.lastTextBaseline)
+    .debugAlignmentGuide(.bottom)
+    .border(.green.tertiary, width: 8)
+
+    DashedDivider()
+
+    Text("Sphinx\nof Black\nQuartz")
+    .font(.largeTitle)
+    .debugAlignmentGuide(.top)
+    .debugAlignmentGuide(.firstTextBaseline)
+    .debugAlignmentGuide(.verticalCenter)
+    .debugAlignmentGuide(.lastTextBaseline)
+    .debugAlignmentGuide(.bottom)
+    .border(.green.tertiary, width: 8)
 }
 
