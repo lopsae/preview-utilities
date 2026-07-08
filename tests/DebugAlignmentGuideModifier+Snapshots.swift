d7f9f81afb395ae64da62849fbc00670dd5f9958
Maic Lopez Saenz <maic@lopsae.com>
2026-07-08 16:49:35 -0700

    add and use assertImageSnapshot

--- tests/DebugAlignmentGuideModifier+Snapshots.swift
+++ tests/DebugAlignmentGuideModifier+Snapshots.swift
@@ -22,21 +22,57 @@ struct DebugAlignmentGuideModifierSnapshots {
         .debugAlignmentGuide(horizontal: .trailing)
         .frame(squareOf: 200)
 
+        // FIXME: add a custom assert using verifySnapshot that builds a view and frames it.
         assertSnapshot(of: view, as: .image, record: .never)
     }
 
 
     @Test func verticalAlignments() {
-        let view = Text("Sphinx\nof Black\nQuartz")
-        .font(.largeTitle)
-        .debugAlignmentGuide(vertical: .top)
-        .debugAlignmentGuide(vertical: .firstTextBaseline)
-        .debugAlignmentGuide(vertical: .center)
-        .debugAlignmentGuide(vertical: .lastTextBaseline)
-        .debugAlignmentGuide(vertical: .bottom)
-        .frame(squareOf: 200)
-
-        assertSnapshot(of: view, as: .image, record: .never)
+        assertImageSnapshot(named: "alignments", record: .never) {
+            Text("Sphinx\nof Black\nQuartz")
+            .font(.largeTitle)
+            .debugAlignmentGuide(vertical: .top)
+            .debugAlignmentGuide(vertical: .firstTextBaseline)
+            .debugAlignmentGuide(vertical: .center)
+            .debugAlignmentGuide(vertical: .lastTextBaseline)
+            .debugAlignmentGuide(vertical: .bottom)
+        }
     }
 
+}
+
+func assertImageSnapshot<Content: View/*, Format*/>(
+    named name: String? = nil,
+    layout: SwiftUISnapshotLayout = .fixed(width: 200, height: 200),
+    record: SnapshotTestingConfiguration.Record? = nil,
+    timeout: TimeInterval = 5,
+    fileID: StaticString = #fileID,
+    file filePath: StaticString = #filePath,
+    testName: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    @ViewBuilder content: () -> Content
+) {
+    let failure = verifySnapshot(
+      of: content(),
+      as: .image(layout: layout),
+      named: name,
+      record: record,
+      timeout: timeout,
+      fileID: fileID,
+      file: filePath,
+      testName: testName,
+      line: line,
+      column: column
+    )
+    guard let message = failure else { return }
+    Issue.record(
+      Comment(rawValue: message),
+      sourceLocation: SourceLocation(
+        fileID: fileID.description,
+        filePath: filePath.description,
+        line: Int(line),
+        column: Int(column)
+      )
+    )
 }
