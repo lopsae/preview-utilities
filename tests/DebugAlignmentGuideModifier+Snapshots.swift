e10ecc03815262919290f684431d3af164cca8f0
Maic Lopez Saenz <maic@lopsae.com>
2026-07-08 23:08:58 -0700

    move assertImageSnapshot to its own file

--- tests/DebugAlignmentGuideModifier+Snapshots.swift
+++ tests/DebugAlignmentGuideModifier+Snapshots.swift
@@ -4,12 +4,10 @@
 //
 
 
-// FIXME: see if testable can be removed once assetImageSnapshot is moved to another file.
 @testable import PreviewUtilities
 
 import SwiftUI
 import Testing
-import SnapshotTesting
 
 
 @MainActor
@@ -119,69 +117,4 @@ private extension View {
         }
     }
 
-}
-
-
-func assertImageSnapshot<Content: View>(
-    named name: String? = nil,
-    layout: SwiftUISnapshotLayout = .fixed(width: 200, height: 200),
-    record: SnapshotTestingConfiguration.Record? = nil,
-    timeout: TimeInterval = 5,
-    fileID: StaticString = #fileID,
-    file filePath: StaticString = #filePath,
-    testName: String = #function,
-    line: UInt = #line,
-    column: UInt = #column,
-    @ViewBuilder content: () -> Content
-) {
-
-    // 1. Get the directory of the current test file
-    let fileURL = URL(fileURLWithPath: "\(filePath)")
-    let testFolderName = "testsx"
-    let maxDeletions: Int = 4
-    guard let testsFolder = fileURL.deletingPathComponents(until: "tests", maxDeletions: maxDeletions) else {
-        Issue.record(
-          "The root tests folder `\(testFolderName)` could not be found within \(maxDeletions) folders of the test suite file",
-          sourceLocation: SourceLocation(
-            fileID: fileID.description,
-            filePath: filePath.description,
-            line: Int(line),
-            column: Int(column)
-          )
-        )
-        return
-    }
-
-    let snapshotsSuffix = "+Snapshots"
-    var testSuiteName = fileURL.deletingPathExtension().lastPathComponent
-    if testSuiteName.hasSuffix("+Snapshots") {
-        testSuiteName.removeLast(snapshotsSuffix.count)
-    }
-
-    let snapshotsFolderName = "recorded-snapshots"
-    let snapshotsFolder = testsFolder.appending(pathComponents: [snapshotsFolderName, testSuiteName])
-
-    let failure = verifySnapshot(
-      of: content(),
-      as: .image(layout: layout),
-      named: name,
-      record: record,
-      snapshotDirectory: snapshotsFolder.path(),
-      timeout: timeout,
-      fileID: fileID,
-      file: filePath,
-      testName: testName,
-      line: line,
-      column: column
-    )
-    guard let message = failure else { return }
-    Issue.record(
-      Comment(rawValue: message),
-      sourceLocation: SourceLocation(
-        fileID: fileID.description,
-        filePath: filePath.description,
-        line: Int(line),
-        column: Int(column)
-      )
-    )
 }
---
+++ tests/utilities/AssertImageSnapshot.swift
@@ -0,0 +1,76 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+@testable import PreviewUtilities
+
+
+import SwiftUI
+import SnapshotTesting
+import Testing
+
+
+func assertImageSnapshot<Content: View>(
+    named name: String? = nil,
+    layout: SwiftUISnapshotLayout = .fixed(width: 200, height: 200),
+    record: SnapshotTestingConfiguration.Record? = nil,
+    timeout: TimeInterval = 5,
+    fileID: StaticString = #fileID,
+    file filePath: StaticString = #filePath,
+    testName: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    @ViewBuilder content: () -> Content
+) {
+
+    let fileURL = URL(fileURLWithPath: "\(filePath)")
+    let testFolderName = "testsx"
+    let maxDeletions: Int = 4
+    guard let testsFolder = fileURL.deletingPathComponents(until: "tests", maxDeletions: maxDeletions) else {
+        Issue.record(
+          "The root tests folder `\(testFolderName)` could not be found within \(maxDeletions) folders of the test suite file",
+          sourceLocation: SourceLocation(
+            fileID: fileID.description,
+            filePath: filePath.description,
+            line: Int(line),
+            column: Int(column)
+          )
+        )
+        return
+    }
+
+    let snapshotsSuffix = "+Snapshots"
+    var testSuiteName = fileURL.deletingPathExtension().lastPathComponent
+    if testSuiteName.hasSuffix("+Snapshots") {
+        testSuiteName.removeLast(snapshotsSuffix.count)
+    }
+
+    let snapshotsFolderName = "recorded-snapshots"
+    let snapshotsFolder = testsFolder.appending(pathComponents: [snapshotsFolderName, testSuiteName])
+
+    let failure = verifySnapshot(
+      of: content(),
+      as: .image(layout: layout),
+      named: name,
+      record: record,
+      snapshotDirectory: snapshotsFolder.path(),
+      timeout: timeout,
+      fileID: fileID,
+      file: filePath,
+      testName: testName,
+      line: line,
+      column: column
+    )
+    guard let message = failure else { return }
+    Issue.record(
+      Comment(rawValue: message),
+      sourceLocation: SourceLocation(
+        fileID: fileID.description,
+        filePath: filePath.description,
+        line: Int(line),
+        column: Int(column)
+      )
+    )
+}
