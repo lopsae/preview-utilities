ef7f653deb8ca00953e698453baf6aee85578c00
Maic Lopez Saenz <maic@lopsae.com>
2026-07-11 20:44:52 -0700

    add snapshot tests for DashedDivider

---
+++ tests/DashedDivider+Snapshots.swift
@@ -0,0 +1,42 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+@testable import PreviewUtilities
+
+import SwiftUI
+import Testing
+
+
+@MainActor
+struct DashedDividerSnapshots {
+
+    @Test func lineWidth() {
+        Snapshots.assertView("horizontal", record: .missing) {
+            VStack(spacing: 20) {
+                DashedDivider()
+                DashedDivider(lineWidth: 2)
+                DashedDivider(lineWidth: 4)
+                DashedDivider(lineWidth: 8)
+            }
+            .padding(.vertical, 16)
+            .border(.red.secondary)
+            .padding(.horizontal, 16)
+        }
+
+        Snapshots.assertView("vertical", record: .missing) {
+            HStack(spacing: 20) {
+                DashedDivider(axis: .vertical)
+                DashedDivider(axis: .vertical, lineWidth: 2)
+                DashedDivider(axis: .vertical, lineWidth: 4)
+                DashedDivider(axis: .vertical, lineWidth: 8)
+            }
+            .padding(.horizontal, 16)
+            .border(.red.secondary)
+            .padding(.vertical, 16)
+        }
+    }
+
+}
