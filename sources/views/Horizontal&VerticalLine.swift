eaec1302480e30d4e1df5ef1621528d40630ebf3
Maic Lopez Saenz <maic@lopsae.com>
2026-07-12 10:38:07 -0700

    implement AxialLine and previews

--- sources/views/Horizontal&VerticalLine.swift
+++ sources/views/Horizontal&VerticalLine.swift
@@ -9,6 +9,69 @@ import SwiftUI
 // FIXME: try to make abstraction using axis.
 
 
+struct AxialLine: Shape {
+
+    let axis: Axis
+    let extendToEdges: Bool
+
+    init(_ axis: Axis, extendToEdges: Bool = false) {
+        self.axis = axis
+        self.extendToEdges = extendToEdges
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let halfPerpendicular = rect.size.length(on: axis.perpendicular) / 2
+        let distanceToEdge = extendToEdges ? .zero : halfPerpendicular
+        let length = rect.size.length(on: axis) - distanceToEdge * 2
+
+        let startPoint = CGPoint(on: axis, distance: distanceToEdge, perpendicular: halfPerpendicular)
+        let endPoint = startPoint.offset(on: axis, distance: length)
+
+        var path = Path()
+        path.move(to: startPoint)
+        path.addLine(to: endPoint)
+        return path
+    }
+
+}
+
+
+private extension CGPoint {
+
+    nonisolated
+    init(on axis: Axis, distance: CGFloat, perpendicular: CGFloat) {
+        switch axis {
+        case .horizontal: self.init(x: distance,      y: perpendicular)
+        case .vertical:   self.init(x: perpendicular, y: distance)
+        }
+    }
+
+    nonisolated
+    func offset(on axis: Axis, distance: CGFloat) -> Self {
+        switch axis {
+        case .horizontal: self.offset(x: distance)
+        case .vertical:   self.offset(y: distance)
+        }
+    }
+
+}
+
+
+private extension CGSize {
+
+    nonisolated
+    func length(on axis: Axis) -> CGFloat {
+        switch axis {
+        case .horizontal:
+            width
+        case .vertical:
+            height
+        }
+    }
+
+}
+
+
 /// Shape with a horizontal line drawn in the middle of its bounds.
 ///
 /// The line path extends horizontally in the middle of the shape bounds. The start and end points
@@ -58,4 +121,45 @@ struct VerticalLine: Shape {
         path.addLineTo(x: halfWidth, y: rect.height - edge)
         return path
     }
+}
+
+
+// MARK: - PreviewContent
+
+
+@MainActor
+private struct PreviewContent {
+
+    static let layout: PreviewTrait<Preview.ViewTraits> = .iPhoneProSizeLayout
+
+}
+
+
+// MARK: - Previews
+
+
+#Preview("Default", traits: .paddingSpacing, .headerFooter, PreviewContent.layout) {
+    AxialLine(.horizontal)
+        .stroke(.red.secondary, lineWidth: 2)
+        .frame(height: 10)
+        .padding()
+        .debugOverlay(.hairline)
+    AxialLine(.vertical)
+        .stroke(.red.secondary, lineWidth: 2)
+        .frame(width: 10)
+        .padding()
+        .debugOverlay(.hairline)
+}
+
+
+#Preview("StrokeStyle", traits: .paddingSpacing, .headerFooter, PreviewContent.layout) {
+    let strokeStyle = StrokeStyle(lineWidth: 40, lineCap: .round)
+    AxialLine(.horizontal)
+        .stroke(.red.secondary, style: strokeStyle)
+        .frame(height: 40)
+        .border(.green.tertiary, width: 4)
+    AxialLine(.vertical)
+        .stroke(.red.secondary, style: strokeStyle)
+        .frame(width: 40)
+        .border(.green.tertiary, width: 4)
 }
