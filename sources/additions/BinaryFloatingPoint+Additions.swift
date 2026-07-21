8809679b06545320e5e5ad50523b6fd833f6ff07
Maic Lopez Saenz <maic@lopsae.com>
2026-07-21 15:45:26 -0700

    add BinaryInteger.one

--- sources/additions/BinaryFloatingPoint+Additions.swift
+++ sources/additions/BinaryFloatingPoint+Additions.swift
@@ -4,10 +4,22 @@
 //
 
 
-nonisolated
 extension BinaryFloatingPoint {
 
+    nonisolated
     static var one: Self { Self(1) }
+
+    nonisolated
     static var half: Self { Self(0.5) }
+
+}
+
+
+// FIXME: Move to its own file.
+
+extension BinaryInteger {
+
+    nonisolated
+    static var one: Self { Self(1) }
 
 }
