8ab5ee2263434c932c97b494d1f4ad84495483ba
Maic Lopez Saenz <maic@lopsae.com>
2026-07-21 16:46:42 -0700

    cleanup documentation

--- sources/miscelaneous/ConfigurationModifier&Trait.swift
+++ sources/miscelaneous/ConfigurationModifier&Trait.swift
@@ -7,33 +7,16 @@
 // FIXME: add tests with a dummy configuration.
 
 
-/// Modifications to a configuration instance.
-///
-/// Modifier instances apply a modification to an instance of type `Configuration`.
-///
-/// ``ConfigurationTrait`` uses modifiers as building blocks for customizing a configuration instance.
-///
-/// Usually a modifier is created for each customizable property of a configuration. This modifiers
-/// should be defined along the configuration implementation, or in a container type to group them
-/// together. It is not advised to place the modifier implementation in the ``ConfigurationTrait``
-/// extension, as the type names may conflict easily with modifiers of other types.
-public protocol ConfigurationModifier<Configuration>: Sendable {
-    associatedtype Configuration
-    func update(configuration: inout Configuration)
-}
-
-
 /// Customization that can be applied to an instance of type `Configuration`.
 ///
 /// Traits are used to build a configuration by applying either a modifier or a collection of other
 /// traits to a configuration instance. Usually a collection of traits is passed as a variadic
-/// parameter to a function that uses those trait to generate a configuration instance, to then
-/// consume that configuration.
+/// parameter to a function that uses those traits to generate a configuration instance and then
+/// consume it.
 ///
-/// passed to a view modifier method to build its configuration. All passed traits
-/// are applied in order to a default configuration, each trait making a modification towards
-/// the final configuration. If multiple traits modify the same configuration properties, the
-/// last one applied may overwrite former traits.
+/// All passed traits are applied in order to a configuration instance, each trait making a
+/// modification. If multiple traits modify the same configuration properties, the last one applied
+/// may overwrite former traits.
 ///
 /// Trait convenience members are declared in extensions constrained to a specific
 /// configuration:
@@ -69,6 +52,29 @@ public enum ConfigurationTrait<Configuration>: Sendable {
     }
 
 }
+
+
+// MARK: - Modifier
+
+
+/// Modifications to a configuration instance.
+///
+/// Modifier instances apply a modification to an instance of type `Configuration`.
+///
+/// ``ConfigurationTrait`` can use modifiers as building blocks for customizing a configuration
+/// instance.
+///
+/// Usually a modifier is created for each customizable property of a configuration. This modifiers
+/// should be defined along the configuration implementation, or in a container type to group them
+/// together. It is not advised to place the modifier implementation in the ``ConfigurationTrait``
+/// extension, as the type names may conflict easily with modifiers of other types.
+public protocol ConfigurationModifier<Configuration>: Sendable {
+    associatedtype Configuration
+    func update(configuration: inout Configuration)
+}
+
+
+// MARK: - TraitConfigurable
 
 
 /// A configuration that can be built by applying ``ConfigurationTrait`` instances to a
