c3e4c54edd21c5f88a2d4d69443df09ca396d76c
Maic Lopez Saenz <maic@lopsae.com>
2026-07-21 18:23:25 -0700

    update the PreviewContent template with Silver

--- sources/miscelaneous/PreviewContentTemplate.swift
+++ sources/miscelaneous/PreviewContentTemplate.swift
@@ -18,9 +18,11 @@ private struct PreviewContent {
 
     static let layout: PreviewTrait<Preview.ViewTraits> = .iPhoneProSizeLayout
 
-    struct ExampleView: View {
+    struct Silver: View {
         var body: some View {
-            CaptionRectangle("Example Preview", color: .orange, size: [200, 150])
+            Text("Ag")
+            .font(.title.pointSize(100))
+            .border(.green.tertiary, width: 8)
         }
     }
 
@@ -31,5 +33,6 @@ private struct PreviewContent {
 
 
 #Preview("Default", traits: .headerFooter, PreviewContent.layout) {
-    PreviewContent.ExampleView()
+    PreviewContent.Silver()
+    .debugOverlay(.size)
 }
