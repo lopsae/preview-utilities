a9ad05a09f9e5259faa541356175690c1d4178e4
Maic Lopez Saenz <maic@lopsae.com>
2026-08-14 07:58:25 -0700

    separate DebugAxisAlignmentGuideModifier to its own file

---
+++ sources/modifiers/DebugAxisAlignmentGuideModifier.swift
@@ -0,0 +1,379 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+import SwiftUI
+
+
+/// Overlays a visual representations of a view's horizontal or vertical alignment guide.
+///
+/// Displays in an overlay a visual representation of a view's alignment guide for a single axis,
+/// either horizontal or vertical. All content added by this modifier is layered in an overlay of
+/// the owner view, the original layout is never modified.
+///
+/// Apply this modifier using ``SwiftUICore/View/debugAlignmentGuide(horizontal:_:)`` or
+/// ``SwiftUICore/View/debugAlignmentGuide(vertical:_:)``:
+///
+/// ```swift
+/// Text("Sphinx of Black Quartz\nJudge my Vow")
+/// .font(.title)
+/// .debugAlignmentGuide(horizontal: .trailing)
+/// ```
+/// ![Text displaying a horizontal trailing alignment guide with the default configuration.](debug-alignment-guide-default-single-axis)
+///
+///
+/// ### Traits and Configuration
+///
+/// The overlay can be configured by passing [`Trait`](doc:DebugAxisAlignmentGuideModifier/Trait) instances to
+/// ``SwiftUICore/View/debugAlignmentGuide(horizontal:_:)`` or ``SwiftUICore/View/debugAlignmentGuide(vertical:_:)``:
+///
+/// ```swift
+/// Text("Lately I saw a house.\nIt was burning.")
+/// .font(.title)
+/// .multilineTextAlignment(.center)
+/// .debugAlignmentGuide(
+///     horizontal: .leading,
+///     .style(.mint.secondary), // Styles markers to mint.
+///     .lineWidth(8),           // Sets the line width.
+///     .scaledLength(2)         // Scales the marker to double the owner size.
+/// )
+/// ```
+/// ![Text displaying a horizontal leading alignment guide using example traits.](debug-alignment-guide-explained-traits-single-axis)
+///
+///
+/// ## Aliases and Protocols
+///
+/// This modifier is setup to receive as a generic the alignment to work with, either
+/// `HorizontalAlignment` or `VerticalAlignment`. Convenience aliases for the typed modifiers are
+/// available at ``DebugHorizontalAlignmentGuideModifier`` and ``DebugVerticalAlignmentGuideModifier``.
+///
+/// The traits for this modifier are defined around a shared protocol ``DebugAxisAlignmentGuideConfigurationProtocol``
+/// to share a single implementation for both vertical and horizontal alignment. The traits are
+/// ultimately applied to the single implementation of this protocol: ``DebugAxisAlignmentGuideConfiguration``.
+public struct DebugAxisAlignmentGuideModifier<AxisAlignment>: ViewModifier
+where
+    AxisAlignment: AlignmentWithOrthogonal,
+    AxisAlignment.OrthogonalAlignment: AlignmentWithDefault
+{
+
+    /// The concrete type implementing `DebugAxisAlignmentGuideConfigurationProtocol`.
+    public typealias ConcreteConfiguration = DebugAxisAlignmentGuideConfiguration<AxisAlignment>
+
+    /// The trait type using `ConcreteConfiguration`.
+    public typealias ConcreteTrait = ConfigurationTrait<ConcreteConfiguration>
+
+    let axisAlignment: AxisAlignment
+    let configuration: ConcreteConfiguration
+
+    @_documentation(visibility: internal)
+    public func body(content: Content) -> some View {
+        let alignment = axisAlignment.alignment(withOrthogonal: configuration.anchor)
+        content.overlay(alignment: alignment) {
+            let axis = axisAlignment.axis
+            let orthogonal = axis.orthogonal
+            GeometryReader { geometry in
+                let baseSize: CGSize = switch configuration.length {
+                case .container, .extended: geometry.size
+                case .fixed(let length):
+                    geometry.size.setting(length: length, along: orthogonal)
+                case .scaled(let multiplier):
+                    geometry.size.multiplying(by: multiplier, along: orthogonal)
+                }
+                let additionalSize: CGSize = switch configuration.length {
+                case .container, .fixed, .scaled: .zero
+                case .extended(let addition):
+                    orthogonal.unitSize.multiplying(by: addition)
+                }
+
+                // Final mark size is clamped to zero. Negative sizes produce a warning.
+                let markSize = baseSize.adding(size: additionalSize).enveloping(.zero)
+
+                AxialLine(orthogonal, style: configuration.shapeStyle, lineWidth: configuration.lineWidth)
+                .frame(size: markSize)
+                .frame(size: geometry.size, alignment: alignment)
+            }
+            .frame(length: configuration.lineWidth, along: axisAlignment.axis)
+            .opacity(configuration.opacity)
+            .allowsHitTesting(false)
+        }
+    }
+
+}
+
+
+/// Convenience alias of `DebugAxisAlignmentGuideModifier` for `HorizontalAlignment`.
+public typealias DebugHorizontalAlignmentGuideModifier = DebugAxisAlignmentGuideModifier<HorizontalAlignment>
+/// Convenience alias of `DebugAxisAlignmentGuideModifier` for `VerticalAlignment`.
+public typealias DebugVerticalAlignmentGuideModifier   = DebugAxisAlignmentGuideModifier<VerticalAlignment>
+
+
+// MARK: - Configuration
+
+
+/// Protocol for the configuration of a `DebugAxisAlignmentGuideModifier`.
+///
+/// Provides the protocol for the configuration of both horizontal and vertical alignments
+/// for ``DebugAxisAlignmentGuideModifier``.
+///
+/// [Traits](doc:DebugAxisAlignmentGuideModifier/Trait) for this modifier are defined in terms of
+/// this protocol, so that the same implementation is available for both horizontal and vertical
+/// alignments.
+///
+/// The modifier uses the implementing type ``DebugAxisAlignmentGuideConfiguration``.
+nonisolated
+public protocol DebugAxisAlignmentGuideConfigurationProtocol: Sendable {
+
+    /// The alignment type to which the alignment marker is itself aligned.
+    ///
+    /// When the size of the alignment marker is customized through ``length``, the marker itself
+    /// can be aligned to this anchor alignment of the owner view.
+    ///
+    /// The anchor alignment is expected to be the ``AlignmentWithOrthogonal/OrthogonalAlignment``
+    /// the alignment type being visualized.
+    associatedtype AnchorAlignment: AlignmentWithDefault
+
+    /// The opacity of the alignment marker.
+    var opacity: Double { get set }
+    /// The shape style of the alignment marker line.
+    var shapeStyle: AnyShapeStyle { get set }
+    /// The line width of the alignment marker line.
+    var lineWidth: CGFloat { get set }
+    /// The length customization applied to the alignment marker.
+    var length: DebugAxisAlignmentConfigurationLength { get set }
+    /// The alignment to which the alignment marker is itself aligned.
+    var anchor: AnchorAlignment { get set }
+
+}
+
+
+/// Concrete implementation of `DebugAxisAlignmentGuideConfigurationProtocol`.
+///
+/// Implementation of ``DebugAxisAlignmentGuideConfigurationProtocol`` used as configuration for
+/// ``DebugAxisAlignmentGuideModifier``. A new instance contains the default configuration for the
+/// modifier.
+nonisolated
+public struct DebugAxisAlignmentGuideConfiguration<AxisAlignment>: DebugAxisAlignmentGuideConfigurationProtocol
+where
+    AxisAlignment: AlignmentWithOrthogonal,
+    AxisAlignment.OrthogonalAlignment: AlignmentWithDefault
+{
+    public typealias AnchorAlignment = AxisAlignment.OrthogonalAlignment
+    public var opacity: Double = .one
+    public var shapeStyle: AnyShapeStyle = AnyShapeStyle(.red.secondary)
+    public var lineWidth: CGFloat = 2
+    public var length: DebugAxisAlignmentConfigurationLength = .container
+    public var anchor: AnchorAlignment = .default
+    public init() {}
+}
+
+
+extension DebugAxisAlignmentGuideConfiguration: TraitInitializable {
+    // init() already defined in struct declaration.
+}
+
+
+// MARK: - ConfigurationLength
+
+
+/// Customization options for the alignment markers of `DebugAlignmentGuideModifier` and
+/// `DebugAxisAlignmentGuideModifier`.
+public enum DebugAxisAlignmentConfigurationLength {
+
+    /// The alignment guide marker occupies the size of the owner view, along the alignment
+    /// orthogonal axis.
+    ///
+    /// This is the default behavior for the alignment guide marker.
+    case container
+
+    /// The alignment guide marker occupies a fixed length along the alignment orthogonal axis.
+    case fixed(CGFloat)
+
+    /// The alignment guide marker occupies the size of the owner view plus the given length, along
+    /// the alignment orthogonal axis.
+    case extended(CGFloat)
+
+    /// The alignment guide marker occupies the size of the owner view multiplied by the given
+    /// factor, along the alignment orthogonal axis.
+    case scaled(CGFloat)
+}
+
+
+// MARK: - Single Axis Traits
+
+
+extension DebugAxisAlignmentGuideModifier {
+
+    /// Customizations that can be applied to the configuration of a `DebugAxisAlignmentGuideModifier`.
+    ///
+    /// ## Topics
+    ///
+    /// ### Visibility Traits
+    /// + ``ConfigurationTrait/hidden-eo3z``
+    /// + ``ConfigurationTrait/visible(_:)->ConfigurationTrait<Configuration>``
+    /// + ``ConfigurationTrait/opacity(_:)->ConfigurationTrait<Configuration>``
+    ///
+    /// ### Style Traits
+    /// + ``ConfigurationTrait/style(_:)->ConfigurationTrait<Configuration>``
+    /// + ``ConfigurationTrait/lineWidth(_:)->ConfigurationTrait<Configuration>``
+    ///
+    /// ### Length Traits
+    /// + ``ConfigurationTrait/length(_:)->ConfigurationTrait<Configuration>``
+    /// + ``ConfigurationTrait/containerLength-589pa``
+    /// + ``ConfigurationTrait/fixedLength(_:)->ConfigurationTrait<Configuration>``
+    /// + ``ConfigurationTrait/extendedLength(_:)->ConfigurationTrait<Configuration>``
+    /// + ``ConfigurationTrait/scaledLength(_:)->ConfigurationTrait<Configuration>``
+    /// + ``ConfigurationTrait/anchor(_:)->ConfigurationTrait<Configuration>``
+    public typealias Trait<C> = ConfigurationTrait<C> where C: DebugAxisAlignmentGuideConfigurationProtocol
+
+}
+
+
+/// Contains the configuration traits that can be applied to the configuration of ``DebugAxisAlignmentGuideModifier``
+/// for both horizontal and vertical alignments.
+
+//extension ConfigurationTrait where Configuration: DebugAxisAlignmentGuideConfigurationProtocol {
+extension DebugAxisAlignmentGuideModifier.Trait where Configuration: DebugAxisAlignmentGuideConfigurationProtocol {
+
+    // FIXME: document.
+    public static var hidden: Self {
+        .modifier(DebugAxisAlignmentModifiers.Opacity(opacity: .zero))
+    }
+
+    // FIXME: document.
+    public static func visible(_ isVisible: Bool) -> Self {
+        .modifier(DebugAxisAlignmentModifiers.Opacity(opacity: isVisible ? .one : .zero))
+    }
+
+    // FIXME: document.
+    public static func opacity(_ opacity: Double) -> Self {
+        .modifier(DebugAxisAlignmentModifiers.Opacity(opacity: opacity))
+    }
+
+
+    // FIXME: document.
+    public static func style(_ style: some ShapeStyle) -> Self {
+        .mutate { $0.shapeStyle = AnyShapeStyle(style) }
+    }
+
+    // FIXME: document.
+    public static func lineWidth(_ lineWidth: CGFloat) -> Self {
+        .mutate { $0.lineWidth = lineWidth }
+    }
+
+    // FIXME: document.
+    public static func length(_ length: DebugAxisAlignmentConfigurationLength) -> Self {
+        .mutate { $0.length = length }
+    }
+
+    // FIXME: document.
+    public static var containerLength: Self {
+        .length(.container)
+    }
+
+    // FIXME: document.
+    public static func fixedLength(_ length: CGFloat) -> Self {
+        .length(.fixed(length))
+    }
+
+    // FIXME: document.
+    public static func extendedLength(_ addition: CGFloat) -> Self {
+        .length(.extended(addition))
+    }
+
+    // FIXME: document.
+    public static func scaledLength(_ factor: CGFloat) -> Self {
+        .length(.scaled(factor))
+    }
+
+    // FIXME: document.
+    public static func anchor(_ anchor: Configuration.AnchorAlignment) -> Self {
+        .modifier(DebugAxisAlignmentModifiers.Anchor(anchor: anchor))
+    }
+
+}
+
+
+/// Container type for modifiers for `DebugAxisAlignmentGuideModifier` configurations.
+///
+/// Contains the modifiers for the configuration of `DebugAxisAlignmentGuideModifier` for both
+/// horizontal and vertical alignments.
+enum DebugAxisAlignmentModifiers<Configuration: DebugAxisAlignmentGuideConfigurationProtocol> {
+
+    struct Opacity: ConfigurationModifier {
+        let opacity: Double
+        func modify(configuration: inout Configuration) {
+            configuration.opacity = opacity
+        }
+    }
+
+    struct Anchor: ConfigurationModifier {
+        let anchor: Configuration.AnchorAlignment
+        func modify(configuration: inout Configuration) {
+            configuration.anchor = anchor
+        }
+    }
+
+}
+
+
+// MARK: - View Extensions
+
+
+extension View {
+
+    /// Layers in front of this view a visual representation of the given horizontal alignment
+    /// guide, customized with the given traits.
+    ///
+    /// Applies the ``DebugHorizontalAlignmentGuideModifier`` customized with the given [`Trait`](doc:DebugAxisAlignmentGuideModifier/Trait)
+    /// instances, overlaying a visual representation of the given horizontal alignment.
+    /// 
+    /// The traits are applied in the order they are passed to a default configuration. Later
+    /// traits may override earlier ones depending on the configuration each trait modifies.
+    /// 
+    /// - Parameters:
+    ///   - horizontalAlignment: The horizontal alignment to visualize.
+    ///   - traits: The traits to customize the default configuration.
+    ///
+    /// - Returns: A view with a configured horizontal alignment guide visualization as foreground.
+    public func debugAlignmentGuide(
+        horizontal horizontalAlignment: HorizontalAlignment,
+        _ traits: DebugHorizontalAlignmentGuideModifier.ConcreteTrait...
+    ) -> some View {
+        let configuration = DebugHorizontalAlignmentGuideModifier.ConcreteConfiguration(traits: traits)
+        let guideModifier = DebugAxisAlignmentGuideModifier(
+            axisAlignment: horizontalAlignment,
+            configuration: configuration
+        )
+        return modifier(guideModifier)
+    }
+
+
+    /// Layers in front of this view a visual representation of the given vertical alignment
+    /// guide, customized with the given traits.
+    ///
+    /// Applies the ``DebugVerticalAlignmentGuideModifier`` customized with the given [`Trait`](doc:DebugAxisAlignmentGuideModifier/Trait)
+    /// instances, overlaying a visual representation of the given vertical alignment.
+    ///
+    /// The traits are applied in the order they are passed to a default configuration. Later
+    /// traits may override earlier ones depending on the configuration each trait modifies.
+    ///
+    /// - Parameters:
+    ///   - verticalAlignment: The vertical alignment to visualize.
+    ///   - traits: The traits to customize the default configuration.
+    ///
+    /// - Returns: A view with a configured horizontal alignment guide visualization as foreground.
+    public func debugAlignmentGuide(
+        vertical verticalAlignment: VerticalAlignment,
+        _ traits: DebugVerticalAlignmentGuideModifier.ConcreteTrait...
+    ) -> some View {
+        let configuration = DebugVerticalAlignmentGuideModifier.ConcreteConfiguration(traits: traits)
+        let guideModifier = DebugAxisAlignmentGuideModifier(
+            axisAlignment: verticalAlignment,
+            configuration: configuration
+        )
+        return modifier(guideModifier)
+    }
+
+}
