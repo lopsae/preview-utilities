173dd0a3c277700dce0c0d52fedad621a80648dd
Maic Lopez Saenz <maic@lopsae.com>
2026-08-29 12:18:59 -0700

    add and use BaselinedIconLabelStyle

--- sources/modifiers/for-previews/SheetWrapperPreviewModifier.swift
+++ sources/modifiers/for-previews/SheetWrapperPreviewModifier.swift
@@ -9,72 +9,87 @@ import SwiftUI
 
 /// Displays a sheet in a preview environment.
 struct SheetPreview<Content>: View where Content: View {
-        @Namespace var namespace
-        @State var isSheetPresented: Bool = true
-        let content: () -> Content
+    @Namespace var namespace
+    @State var isSheetPresented: Bool = false
+    let content: () -> Content
 
-        let captionKey: LocalizedStringKey?
+    let captionKey: LocalizedStringKey?
 
-        init(
-            _ captionKey: LocalizedStringKey? = nil,
-            @ViewBuilder content: @escaping () -> Content
-        ) {
-            self.captionKey = captionKey
-            self.content = content
+    init(
+        _ captionKey: LocalizedStringKey? = nil,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.captionKey = captionKey
+        self.content = content
+    }
+
+    var body: some View {
+        let sheetTransitionId = "previewSheet.transtionId"
+        VStack {
+            if let captionKey {
+                VStack(spacing: Defaults.padding/3) {
+                    Image(systemName: "info.circle")
+                    .imageScale(.large)
+                    Text(captionKey)
+                    .expandingWidthFrame()
+                    .padding(.not(.top), Defaults.padding*1.5)
+                }
+                .maxWidthFrame()
+                .background {
+                    ConcentricRectangle(
+                        uniformTopCorners: .concentric,
+                        uniformBottomCorners: .concentric(minimum: .fixed(Defaults.padding))
+                    )
+                    .fill(.ultraThinMaterial)
+                    .padding(Defaults.padding/2)
+                    .ignoresSafeArea()
+                }
+            }
+
+            Spacer()
         }
-
-        var body: some View {
-            let sheetTransitionId = "previewSheet.transtionId"
-            VStack {
-                if let captionKey {
-                    VStack(spacing: Defaults.padding/3) {
-                        Image(systemName: "info.circle")
-                        .imageScale(.large)
-                        Text(captionKey)
-                        .expandingWidthFrame()
-                        .padding(.not(.top), Defaults.padding*1.5)
-                    }
-                    .maxWidthFrame()
-                    .background {
-                        ConcentricRectangle(
-                            uniformTopCorners: .concentric,
-                            uniformBottomCorners: .concentric(minimum: .fixed(Defaults.padding))
-                        )
-                        .fill(.ultraThinMaterial)
-                        .padding(Defaults.padding/2)
-                        .ignoresSafeArea()
-                    }
-                }
-
-                Spacer()
+        .maxSizeFrame()
+        .background {
+            PrettyMesh.auroraEgg
+            .ignoresSafeArea()
+        }
+        .safeAreaInset(edge: .bottom) {
+            Button("Show Sheet", constrainedSystemImage: "chevron.up") {
+                isSheetPresented.toggle()
             }
-            .maxSizeFrame()
-            .background {
-                PrettyMesh.auroraEgg
-                .ignoresSafeArea()
-            }
-            .safeAreaInset(edge: .bottom) {
-                Button("Show Sheet", systemImage: "chevron.up") {
-                    isSheetPresented = true
-                }
-                // TODO: Check button formatting.
-//                .buttonStyle(.iconOnlyGlassReady)
-//                .glassEffectInteractiveInCircle()
-                .glassEffect(.regular.interactive(), in: .circle)
-                .matchedTransitionSource(id: sheetTransitionId, in: namespace)
-            }
-            .task {
-                // Without this sleep, the sheet has issues (like displaying the wrong size) when
-                // the preview refreshes.
-                try? await Task.sleep(for: .seconds(0.5))
-                isSheetPresented = true
-            }
-            .sheet(isPresented: $isSheetPresented) {
-                content()
-                .navigationTransition(.zoom(sourceID: sheetTransitionId, in: namespace))
-            }
+            .labelStyle(BaselinedIconLabelStyle(length: 44))
+            .buttonStyle(.plain)
+            .glassEffect(.regular.interactive(), in: .circle)
+            .matchedTransitionSource(id: sheetTransitionId, in: namespace)
+        }
+        .task {
+            // Without this sleep, the sheet has issues (like displaying the wrong size) when
+            // the preview refreshes.
+            try? await Task.sleep(for: .seconds(0.5))
+            isSheetPresented = true
+        }
+        .sheet(isPresented: $isSheetPresented) {
+            content()
+            .navigationTransition(.zoom(sourceID: sheetTransitionId, in: namespace))
         }
     }
+}
+
+
+// FIXME: Seems to have issues with hit testing.
+// TODO: Brough from Trailing{Closure}, consider exposing or moving to its own package.
+struct BaselinedIconLabelStyle: LabelStyle {
+    let length: CGFloat
+    func makeBody(configuration: Configuration) -> some View {
+        Image(systemName: "circle")
+        .hidden()
+        .overlay(alignment: .centerFirstTextBaseline) {
+            configuration.icon
+        }
+        .frame(width: length, height: length)
+    }
+}
+
 
 
 // MARK: - PreviewContent
