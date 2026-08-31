8854542dd1b883c7ac05a40efa0b7284f7b1b1a1
Maic Lopez Saenz <maic@lopsae.com>
2026-08-30 18:28:36 -0700

    add preview to LocalizedStringKey interpolations

--- sources/additions/LocalizedStringKey+Additions.swift
+++ sources/additions/LocalizedStringKey+Additions.swift
@@ -28,9 +28,60 @@ extension LocalizedStringKey.StringInterpolation {
     }
 
 
-    mutating func appendInterpolation(usingNbsp label: String) {
+    mutating func appendInterpolation(nonBreaking label: String) {
         let label = label.replacingOccurrences(of: " ", with: String.nbsp)
         appendInterpolation(label)
     }
 
+}
+
+
+// MARK: - PreviewContent
+
+
+@MainActor
+private struct PreviewContent {
+
+    static let layout: PreviewTrait<Preview.ViewTraits> = .iPhoneProSizeLayout
+
+}
+
+
+// MARK: - Previews
+
+
+#Preview("Default", traits: .fixedHeaderFooter, PreviewContent.layout) {
+    @Previewable @State var fixedWidth: Double = 400
+
+    Slider.captioned("Fixed Width", value: $fixedWidth, in: 0...400, valueFormat: .arithmeticRoundedInteger)
+
+    DashedDivider()
+
+    Text("Test before \(systemImage: "ladybug", label: "Ladybug image yes nbsp") after interpolation")
+    .frame(width: fixedWidth)
+    .floatingCaption("Image+Label+YesNbsp", .colorStyle(.yellow), .alignment(.outerBottomTrailing))
+    .padding(.bottom)
+
+    DashedDivider()
+
+    Text("Test before \(systemImage: "ladybug", label: "Ladybug image no nbsp", useNbsp: false) after interpolation")
+    .frame(width: fixedWidth)
+    .floatingCaption("Image+Label+NoNbsp", .colorStyle(.yellow), .alignment(.outerBottomTrailing))
+    .padding(.bottom)
+
+    DashedDivider()
+
+    Text("Test before \(image: .moduleCatalog(.envelopeOffcenterBadgeTopTrailing), label: "Envelope Image") after interpolation")
+    .frame(width: fixedWidth)
+    .floatingCaption("ImageResource+Label+YesNbsp", .colorStyle(.yellow), .alignment(.outerBottomTrailing))
+    .padding(.bottom)
+
+    DashedDivider()
+
+    Text("Test before \(nonBreaking: "Non breaking text") after interpolation")
+    .frame(width: fixedWidth)
+    .floatingCaption("String+NonBreaking", .colorStyle(.yellow), .alignment(.outerBottomTrailing))
+    .padding(.bottom)
+
+    Spacer()
 }
