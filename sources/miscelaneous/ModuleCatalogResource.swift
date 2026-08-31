50cf4b344419a590c095148ce4a22aaf2947bd1a
Maic Lopez Saenz <maic@lopsae.com>
2026-08-30 18:28:22 -0700

    add note about not using asset catalong convenience accessors

--- sources/miscelaneous/ModuleCatalogResource.swift
+++ sources/miscelaneous/ModuleCatalogResource.swift
@@ -7,6 +7,13 @@ import DeveloperToolsSupport
 
 
 /// Identifiers for image resources bundled in the module asset catalog.
+///
+/// Use these identifiers and `ImageResource/moduleCatalog(_:)` to access the assets in the package
+/// asset catalog, instead of using the convenience accessors provided by Xcode.
+///
+/// When the package is build outside of Xcode, (E.g. using `swift build` in console) the
+/// convenience accessors in ImageResource for assets in the asset catalog are NOT available and
+/// the build will fail. These convenience accessors are only generated through Xcode.
 enum ModuleCatalogResource: String {
     case envelopeOffcenterBadgeTopTrailing    = "custom.envelope.offcenter.badge.top.trailing"
     case envelopeOffcenterBadgeBottomTrailing = "custom.envelope.offcenter.badge.bottom.trailing"
