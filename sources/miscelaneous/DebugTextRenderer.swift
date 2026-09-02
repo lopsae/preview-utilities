373df234389438182419da869267305363df2a4a
Maic Lopez Saenz <maic@lopsae.com>
2026-09-02 08:35:24 -0700

    add configuration object

--- sources/miscelaneous/DebugTextRenderer.swift
+++ sources/miscelaneous/DebugTextRenderer.swift
@@ -10,10 +10,19 @@ import SwiftUI
 /// Draws the typographic bounds of every run.
 struct DebugTextRenderer: TextRenderer {
 
+    let configuration: Configuration
+
+    init(configuration: Configuration = .all) {
+        self.configuration = configuration
+    }
+
     func draw(layout: Text.Layout, in context: inout GraphicsContext) {
         for line in layout {
             for run in line {
-                Self.drawDebugTypographicBounds(run: run, in: context)
+                Self.drawDebugTypographicBounds(
+                    run: run, in: context,
+                    configuration: configuration
+                )
                 context.draw(run)
             }
         }
@@ -22,24 +31,59 @@ struct DebugTextRenderer: TextRenderer {
 
     static func drawDebugTypographicBounds(
         run: Text.Layout.Run,
-        in context: GraphicsContext
+        in context: GraphicsContext,
+        configuration: Configuration
     ) {
         // FIXME: Make debug configuration with shorthands for .all, .none., .rect, .ascentDecent
-        let typo = run.typographicBounds
-        let boundsPath = Rectangle().path(in: typo.rect.inset(by: 0.5))
-        context.stroke(boundsPath, with: .style(.green.secondary))
+        let bounds = run.typographicBounds
 
-        let ascentLine = Path { path in
-            path.move(to: typo.origin.offset(x: 1))
-            path.addLine(to: typo.origin.offset(x: 1, y: -typo.ascent))
+        if configuration.drawsRect {
+            let rectPath = Rectangle().path(in: bounds.rect.inset(by: 0.5))
+            context.stroke(rectPath, with: .style(.green.secondary))
         }
-        context.stroke(ascentLine, with: .style(.red.secondary), lineWidth: 2)
 
-        let descentLine = Path { path in
-            path.move(to: typo.origin.offset(x: 3))
-            path.addLine(to: typo.origin.offset(x: 3, y: typo.descent))
+        if configuration.drawsAscent {
+            let ascentLine = Path { path in
+                path.move(to: bounds.origin.offset(x: 1))
+                path.addLine(to: bounds.origin.offset(x: 1, y: -bounds.ascent))
+            }
+            context.stroke(ascentLine, with: .style(.red.secondary), lineWidth: 2)
         }
-        context.stroke(descentLine, with: .style(.blue.secondary), lineWidth: 2)
+
+        if configuration.drawsDescent {
+            let descentLine = Path { path in
+                path.move(to: bounds.origin.offset(x: 3))
+                path.addLine(to: bounds.origin.offset(x: 3, y: bounds.descent))
+            }
+            context.stroke(descentLine, with: .style(.blue.secondary), lineWidth: 2)
+        }
+    }
+
+}
+
+
+extension DebugTextRenderer {
+
+    struct Configuration {
+        var drawsRect: Bool
+        var drawsAscent: Bool
+        var drawsDescent: Bool
+
+        init(all: Bool = true) {
+            self.drawsRect    = all
+            self.drawsAscent  = all
+            self.drawsDescent = all
+        }
+
+        init(rect: Bool, ascent: Bool, descent: Bool) {
+            self.drawsRect    = rect
+            self.drawsAscent  = ascent
+            self.drawsDescent = descent
+        }
+
+        static let all: Self = .init()
+        static let none: Self = .init(all: false)
+        static let onlyRect: Self = .init(rect: true, ascent: false, descent: false)
     }
 
 }
@@ -69,6 +113,14 @@ private struct PreviewContent {
     Text(Strings.sphinxOfBlackQuartz)
     .textRenderer(DebugTextRenderer())
     .frame(width: fixedWidth)
-    .floatingCaption("Text", .width, .colorStyle(.orange), .alignment(.outerBottomTrailing))
+    .floatingCaption("All", .colorStyle(.orange), .alignment(.outerBottomTrailing))
+    .padding(.bottom)
+
+    DashedDivider()
+
+    Text(Strings.sphinxOfBlackQuartz)
+        .textRenderer(DebugTextRenderer(configuration: .onlyRect))
+    .frame(width: fixedWidth)
+    .floatingCaption("Rects", .colorStyle(.orange), .alignment(.outerBottomTrailing))
     .padding(.bottom)
 }
