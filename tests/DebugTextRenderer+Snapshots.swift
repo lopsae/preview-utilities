664e8a017687d075a3b51fab93f1247230ced001
Maic Lopez Saenz <maic@lopsae.com>
2026-09-02 16:00:35 -0700

    add tests for DebugTextRenderer

---
+++ tests/DebugTextRenderer+Snapshots.swift
@@ -0,0 +1,53 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+@testable import PreviewUtilities
+
+import SwiftUI
+import Testing
+
+
+@MainActor
+struct DebugTextRendererSnapshots {
+
+    enum TestContent {
+        static let multi = Text("""
+            sphinx of
+            black Quartz
+            judge my Vow
+            """
+        ).foregroundStyle(.tertiary)
+    }
+
+    @Test(.snapshots(record: .missing))
+    func defaultConfig() {
+        Snapshots.assertView("body", colorSchemes: .all) {
+            TestContent.multi
+            .textRenderer(DebugTextRenderer())
+        }
+
+        Snapshots.assertView("title", colorSchemes: .all) {
+            TestContent.multi
+            .font(.title)
+            .textRenderer(DebugTextRenderer())
+        }
+    }
+
+
+    @Test(.snapshots(record: .missing))
+    func otherConfigs() {
+        Snapshots.assertView("none") {
+            TestContent.multi
+            .textRenderer(DebugTextRenderer(configuration: .none))
+        }
+
+        Snapshots.assertView("onlyRect") {
+            TestContent.multi
+            .textRenderer(DebugTextRenderer(configuration: .onlyRect))
+        }
+    }
+
+}
