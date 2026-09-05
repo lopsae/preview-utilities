7a6092ac1771e6d26155deee5593724343f07daf
Maic Lopez Saenz <maic@lopsae.com>
2026-09-05 16:55:27 -0700

    move Pathable to its own file

---
+++ sources/geometry/Pathable.swift
@@ -0,0 +1,34 @@
+//
+//  PreviewUtilities
+//  Created by Maic Lopez Saenz.
+//
+
+
+import SwiftUI
+
+
+nonisolated
+protocol Pathable {
+    var path: Path { get }
+}
+
+
+nonisolated
+extension Pathable {
+
+    @discardableResult
+    func add(to container: inout Path) -> Self {
+        container.addPath(path)
+        return self
+    }
+
+    @discardableResult
+    func stroke(
+        in context: GraphicsContext,
+        style: some ShapeStyle,
+        lineWidth: CGFloat = .one
+    ) -> Self {
+        context.stroke(path, with: .style(style), lineWidth: lineWidth)
+        return self
+    }
+}
--- sources/geometry/Segment.swift
+++ sources/geometry/Segment.swift
@@ -7,34 +7,6 @@
 import SwiftUI
 
 
-// FIXME: Move to its own file.
-nonisolated
-protocol Pathable {
-    var path: Path { get }
-}
-
-
-nonisolated
-extension Pathable {
-
-    @discardableResult
-    func add(to container: inout Path) -> Self {
-        container.addPath(path)
-        return self
-    }
-
-    @discardableResult
-    func stroke(
-        in context: GraphicsContext,
-        style: some ShapeStyle,
-        lineWidth: CGFloat = .one
-    ) -> Self {
-        context.stroke(path, with: .style(style), lineWidth: lineWidth)
-        return self
-    }
-}
-
-
 nonisolated
 struct Segment {
     var start: CGPoint
